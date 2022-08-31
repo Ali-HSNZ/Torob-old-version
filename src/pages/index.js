f895944e4f4bb29e4d938fc2ee3afde1a83e81da
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-08-31 23:21:02 +0430

    render Header And Footer in home page => src/pages/index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,29 @@
-import Footer from '@/layout/Footer'
-import Header from '@/layout/Header'
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
 
 export default function Home() {
   return (
-    <section className='absolute top-0 bottom-0  w-full flex justify-center flex-col items-center '>
-        <article className='flex items-center  justify-center px-4'>
-            <div className='w-24'>
-              <img className='w-full h-auto' src='https://storage.irantalent.com/brand-data/brand_data_7903eDd_603e2f4ab6949.png?w=120'/>
-            </div>
-          <div className='mr-4 flex flex-col gap-y-2'>
-            <p className='text-[#d73948] text-[35px] sm:text-[40px] font-sans font-bold'>ترب</p>
-            <p className='text-gray-500 text-sm font-sans'>مقایسه قیمت میلیون ها محصول بین هزاران فروشگاه</p>
-          </div>
-        </article>
-        <section className='mt-8 w-full flex justify-center relative '>
-          <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 relative right-10 top-3" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="gray" >
-              <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-          </svg>
-          <input className='w-9/12 sm:w-[470px] font-sans  h-12 focus:ring-0 text-base pr-12 focus:outline-none focus:border-gray-300 rounded-md border-gray-300 outline-none' type="text" placeholder='نام کالا را وارد کنید'/>
+      <>
+           <Header/>
+            <section className='absolute top-0 bottom-0  w-full flex justify-center flex-col items-center '>
+                <article className='flex items-center  justify-center px-4'>
+                    <div className='w-24'>
+                      <img className='w-full h-auto' src='https://storage.irantalent.com/brand-data/brand_data_7903eDd_603e2f4ab6949.png?w=120'/>
+                    </div>
+                  <div className='mr-4 flex flex-col gap-y-2'>
+                    <p className='text-[#d73948] text-[35px] sm:text-[40px] font-sans font-bold'>ترب</p>
+                    <p className='text-gray-500 text-sm font-sans'>مقایسه قیمت میلیون ها محصول بین هزاران فروشگاه</p>
+                  </div>
+                </article>
+                <section className='mt-8 w-full flex justify-center relative '>
+                  <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 relative right-10 top-3" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="gray" >
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                  </svg>
+                  <input className='w-9/12 sm:w-[470px] font-sans  h-12 focus:ring-0 text-base pr-12 focus:outline-none focus:border-gray-300 rounded-md border-gray-300 outline-none' type="text" placeholder='نام کالا را وارد کنید'/>
 
-        </section>
-    </section>
+                </section>
+            </section>
+            <Footer/>
+      </>
   )
 }
