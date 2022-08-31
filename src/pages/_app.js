bd551ba794f4a057827f0b0b41f5b30ed50569f7
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-08-31 23:17:21 +0430

    Removed Layout of Root Application => src/pages/_app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,7 @@ import Layout from '../layout/Layout'
 import '../../styles/globals.css'
 import 'tailwindcss/tailwind.css'
 function MyApp({ Component, pageProps }) {
-  return <Layout> <Component {...pageProps} /></Layout>
+  return  <Component {...pageProps} />
 }
 
 export default MyApp
