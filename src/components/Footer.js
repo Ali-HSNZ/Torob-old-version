95dab25d005ffa8aa3fe6bbe6f0e6de7e833bce9
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-08-31 23:18:56 +0430

    Moved Header And Footer in This Folder => src/components

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (  
+        <section className="fixed bottom-0 right-0 left-0 flex justify-between px-10 py-2.5 bg-gray-50">
+            <nav className="flex gap-x-4 text-gray-500 gap-3 text-sm items-center flex-wrap">
+                <a>راهنمای خرید امن</a>
+                <a>پیگیری سفارش</a>
+                <a>تماس باما</a>
+                <a>درباره ترب</a>
+                <a>تخفیف ها</a>
+            </nav>
+        </section>
+    );
+}
+ 
+export default Footer;
