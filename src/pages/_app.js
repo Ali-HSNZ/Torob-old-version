72db1f99fc4407c115ad4b48aad229efa6ec0638
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-08-31 19:57:48 +0430

    import 'tailwindcss/tailwind.css' => src/pages/_app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 import Layout from '../layout/Layout'
 import '../../styles/globals.css'
-
+import 'tailwindcss/tailwind.css'
 function MyApp({ Component, pageProps }) {
   return <Layout> <Component {...pageProps} /></Layout>
 }
