b9ba4ffc328c5ce61630b4e70c8ed60e7465de68
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-08-31 23:16:14 +0430

    removed Header and Footer of layout and moved to components=> src/layout

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,12 +1,9 @@
-import Footer from "./Footer";
-import Header from "./Header";
+
 
 const Layout = ({children}) => {
     return (  
         <div>
-            <Header/>
             {children}
-            <Footer/>
         </div>
     );
 }
