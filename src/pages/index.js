08b0841bfc6f058ac6a6dbfb41eabc8773152bbe
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-09-01 19:14:44 +0430

    Handle And Submit form value => src/pages/index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,25 @@
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
 
 export default function Home() {
+  const [value , setValue] = useState("")
+  
+  const router = useRouter()
+
+  const handlePath = (e,inputValue) => {
+    e.preventDefault()
+    router.push({
+      pathname : "/search",
+      search : "query="+inputValue,
+    })
+
+  }
+
   return (
       <>
-           <Header/>
+            <Header/>
             <section className='absolute top-0 bottom-0  w-full flex justify-center flex-col items-center '>
                 <article className='flex items-center  justify-center px-4'>
                     <div className='w-24'>
@@ -16,10 +31,13 @@ export default function Home() {
                   </div>
                 </article>
                 <section className='mt-8 w-full flex justify-center relative '>
-                  <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 relative right-10 top-3" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="gray" >
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                  </svg>
-                  <input className='w-9/12 sm:w-[470px] font-sans  h-12 focus:ring-0 text-base pr-12 focus:outline-none focus:border-gray-300 rounded-md border-gray-300 outline-none' type="text" placeholder='نام کالا را وارد کنید'/>
+                  
+                  <form method='get' className='w-full flex  justify-center' onSubmit={(e)=>handlePath(e,value)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 relative right-10 top-3" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="gray" >
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                    </svg>
+                      <input onChange={input => setValue(input.target.value)} className='w-9/12 sm:w-[470px] font-sans  h-12 focus:ring-0 text-base pr-12 focus:outline-none focus:border-gray-300 rounded-md border-gray-300 outline-none' type="text" placeholder='نام کالا را وارد کنید'/>
+                  </form>
 
                 </section>
             </section>
