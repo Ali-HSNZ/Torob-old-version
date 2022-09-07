0ced58885e5e6f04350f394f7d26e536b3e17007
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-09-07 20:07:57 +0430

    Fixed className Error

--- src/components/searchPage/aside/mobile/MobileBrands.js
+++ src/components/searchPage/aside/mobile/MobileBrands.js
@@ -33,7 +33,7 @@ const MobileBrands = ({closeHandler , brands}) => {
             <div className="px-4 mt-8 flex justify-between">
                 <h6 className="text-sm font-sans">برند ها</h6>
                 <button onClick={()=> closeHandler(false)}>
-                    <svg classname="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" >
+                    <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" >
                         <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </button>
