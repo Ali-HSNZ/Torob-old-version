20cb4bffe8110273ebfb3e83b7acad369e5f99ff
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-09-07 23:57:10 +0430

    Fixed Bugs in Available checkBox => src/components/searchPage/aside/desk/Available.js

--- src/components/searchPage/aside/desk/Available.js
+++ src/components/searchPage/aside/desk/Available.js
@@ -1,10 +1,21 @@
-import Link from "next/link";
+import { useRouter } from "next/router";
 import { useState } from "react";
 
-const Available = ({query}) => {
-
+const Available = () => {
+    const {query} = useRouter()
     const [isQuantityTaggle, setisQuantityTaggle] = useState(false)
     const [isQuantityCheckbox , setIsQuantityCheckbox] = useState(false)
+
+    const router = useRouter()
+    const handler = () =>{
+        if(isQuantityCheckbox){
+            router.push({pathname : "/search" , query : {...query , available:true }})
+        }else{
+            delete query.available; 
+            router.push({pathname : "/search" , query : {...query}})
+        }
+    }
+
 
     return (
         <section className="flex flex-col w-full  px-6">
@@ -16,14 +27,14 @@ const Available = ({query}) => {
                 </div>
                 <span className="font-sans mr-2">موجودی  </span>
             </div>
-            <div className={`${isQuantityTaggle ? "" : "hidden"} pb-3  gap-x-4`}>
-                    <Link href={{pathname : "/search" , query : {...query , available:!isQuantityCheckbox }}}>
+            <section className={`${isQuantityTaggle ? "" : "hidden"} pb-3  gap-x-4`}>
+                    <div onClick={handler}>
                         <a className="flex items-center mb-4" onClick={()=>setIsQuantityCheckbox(!isQuantityCheckbox)} >
                             <input id="default-checkbox" type="checkbox"  checked={query.available} onChange={(e) => setIsQuantityCheckbox(e.target.checked)}  className="w-5 h-5 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
                             <label htmlFor="default-checkbox" className="mr-3 font-sans text-sm  font-medium cursor-pointer text-gray-700 dark:text-gray-300"> نمایش محصولات موجود</label>
                         </a>
-                    </Link>
-            </div>
+                    </div>
+            </section>
         </section>
     );
 }
