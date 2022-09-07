28343564a6f53381437d265eed546db8d0a367d3
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-09-08 00:13:17 +0430

    Added FontFamily in Footer => src/components/Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (  
         <section className="fixed bottom-0 right-0 left-0 flex justify-between px-10 py-2.5 bg-gray-50">
-            <nav className="flex gap-x-4 text-gray-500 gap-3 text-sm items-center flex-wrap">
+            <nav className="flex gap-x-4 text-gray-500 font-sans gap-3 text-sm items-center flex-wrap">
                 <a>راهنمای خرید امن</a>
                 <a>پیگیری سفارش</a>
                 <a>تماس باما</a>
