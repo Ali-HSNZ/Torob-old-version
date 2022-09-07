2a36cd9f19846a4191237b4be3048b7b0c4d966c
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-09-07 19:50:27 +0430

    change page paths in tailwind config => tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     "./node_modules/flowbite-react/**/*.js",
-    "src/pages**/*.{js,ts,jsx,tsx}",
+    "src/pages/**/*.{js,ts,jsx,tsx}",
     "src/components/**/*.{js,ts,jsx,tsx}",
     "src/common/**/*.{js,ts,jsx,tsx}",
     "src/layout/**/*.{js,ts,jsx,tsx}",
