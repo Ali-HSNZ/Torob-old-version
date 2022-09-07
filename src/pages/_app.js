7bc8f9564881ce44b45dad78296901f692f8b580
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-09-07 19:42:06 +0430

    added NextProgress in _app => src/pages/_app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,15 @@
 import Layout from '../layout/Layout'
 import '../../styles/globals.css'
 import 'tailwindcss/tailwind.css'
+import NextNProgress from "nextjs-progressbar";
+
 function MyApp({ Component, pageProps }) {
-  return  <Component {...pageProps} />
+  return  (
+    <>
+        <NextNProgress options={{ easing: "ease", speed: 500 }} color="red" />
+        <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
