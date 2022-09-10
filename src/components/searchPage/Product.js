8e33da1d791fd2987760df636342e5eeec91c096
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-09-11 00:34:42 +0430

    Create Chart & Product & Properties & Shop in Product Page => src/components/searchPage/Product.js

--- src/components/searchPage/Product.js
+++ src/components/searchPage/Product.js
@@ -4,6 +4,7 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import Styles from './product.module.css'
 import ReactLoading from 'react-loading';
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 const Product = ({ data}) => {
   const [products, setProducts] = useState(data);
@@ -56,33 +57,39 @@ useEffect(()=>{
             endMessage={<h4 className="w-full text-center font-sans my-8">محصولات بیشتری یافت نشد</h4>
         }>
         <article className={Styles.productsParent}>
-            {products.map((product,index) => (
-                <div key={index} className="bg-white rounded-md w-full h-auto min-w-[200px] px-4 flex flex-col items-center">
-                    <div className="w-24 lg:w-28 flex justify-center pt-4 ">
-                        <img src={product.image_url} className="w-full h-auto"/>
-                    </div>
-                
-                    <h3 className="font-sans text-sm text-right w-full mt-5">{product.title}</h3>
-                
-                    <div className=" flex flex-col justify-end w-full mt-5 flex-1 gap-y-2">
-                        <h6 className={`font-sans text-sm  ${product.price_start===0 ? "text-red-600" : "text-gray-700"}`}>{product.price_start === 0 ? "ناموجود" :  toPersianDigits(product.price_start)+" تومان " }</h6>
-                        <h6 className="font-sans text-sm text-gray-500">در {toPersianDigits(product.shops_count)} فروشگاه</h6>
-                    </div>
-                    <div className="flex my-4 justify-between w-full px-9">
-                        <div className="p-2 hover:bg-gray-100 rounded-full cursor-pointer">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-gray-500">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-                            </svg>
-                        </div>
-                        <div className="p-2 hover:bg-gray-100 rounded-full cursor-pointer">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-gray-500">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
-                            </svg>
-                        </div>
-                    </div>
+            {products.map((product,index) => {
 
-                </div>
-            ))}
+                return(
+                    <Link href={`/product/${product.hash_id}/${product.title.replace(/\s+/g, '-')}`}>
+                        <a>
+                            <div key={index} className="bg-white rounded-md w-full h-auto min-w-[200px] px-4 flex flex-col items-center">
+                                <div className="w-24 lg:w-28 flex justify-center pt-4 ">
+                                    <img src={product.image_url} className="w-full h-auto"/>
+                                </div>
+                            
+                                <h3 className="font-sans text-sm text-right w-full mt-5">{product.title}</h3>
+                            
+                                <div className=" flex flex-col justify-end w-full mt-5 flex-1 gap-y-2">
+                                    <h6 className={`font-sans text-sm  ${product.price_start===0 ? "text-red-600" : "text-gray-700"}`}>{product.price_start === 0 ? "ناموجود" :  toPersianDigits(product.price_start)+" تومان " }</h6>
+                                    <h6 className="font-sans text-sm text-gray-500">در {toPersianDigits(product.shops_count)} فروشگاه</h6>
+                                </div>
+                                <div className="flex my-4 justify-between w-full px-9">
+                                    <div className="p-2 hover:bg-gray-100 rounded-full cursor-pointer">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-gray-500">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                                        </svg>
+                                    </div>
+                                    <div className="p-2 hover:bg-gray-100 rounded-full cursor-pointer">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-gray-500">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+                                        </svg>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </Link>
+                )
+            })}
         </article>
 
       </InfiniteScroll>
