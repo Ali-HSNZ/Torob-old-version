286deea767216932d1df74188767a39011830d92
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-09-12 00:49:19 +0430

    Refactor Code => src/components/searchPage/main/BreadCrumpAndSort.js

--- src/components/searchPage/main/BreadCrumpAndSort.js
+++ src/components/searchPage/main/BreadCrumpAndSort.js
@@ -1,5 +1,6 @@
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React, { useState } from 'react';
 
@@ -82,42 +83,41 @@ const BreadCrumpAndSort = ({category , similarCategories}) => {
     const subName = returnSubcategory(category).subName && returnSubcategory(category).subName !== category?  returnSubcategory(category).subName  + " / "  : "";
     const sub_subName = returnSubcategory(category).sub_subName && returnSubcategory(category).sub_subName !== category ? returnSubcategory(category).sub_subName  + " / "  : "";
 
-
     return (  
-        <div className={`w-full flex ${category ? " justify-between" : "justify-end"}   z-10 relative items-center`}>
-        {/* //? BreadCrump */}
-            {category && <p className="font-sans py-4 text-xs text-gray-500">همه دسته ها / {`${mainName} ${subName}  ${sub_subName} ${sub_subName === category  ? "" : " / ",category}`}</p>}
-        {/* //? Sort */}
-            <div className="max-w-fit py-2  cursor-pointer hidden  md:flex flex-col justify-end  items-end">
-            
-                <Button id="basic-button" aria-controls={open ? 'basic-menu' : undefined} aria-haspopup="true" aria-expanded={open ? 'true' : undefined} onClick={handleClick} className="font-sans  text-sm text-gray-600 rounded-md">
-                    {currentFilter}
-                    <svg  className="w-5 h-5 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                    </svg>
-                </Button>
-                <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
-                    <div className="flex flex-col  py-1">
-                        <section  onClick={() => router.push({pathname : '/search' , query : {...query , sort : "mostFavorite"}}) &  handleClose() & setCurrentFilter("محبوب‌ ترین")}>
-                            <p className="py-1.5 px-4 w-full cursor-pointer font-sans text-sm text-center hover:bg-gray-100">محبوب‌ ترین</p>
-                        </section>
+            <div className={`w-full flex ${category ? " justify-between" : "justify-end"}   z-10 relative items-center`}>
+                {/* //? BreadCrump */}
+                {query.category &&  <p className="font-sans py-4 text-xs text-gray-500">همه دسته ها / {`${mainName} ${subName}  ${sub_subName} ${sub_subName === category  ? "" : " / ",category}`}</p>}
+                {/* //? Sort */}
+                <div className="max-w-fit py-2  cursor-pointer hidden  md:flex flex-col justify-end  items-end">
+                
+                    <Button id="basic-button" aria-controls={open ? 'basic-menu' : undefined} aria-haspopup="true" aria-expanded={open ? 'true' : undefined} onClick={handleClick} className="font-sans  text-sm text-gray-600 rounded-md">
+                        {currentFilter}
+                        <svg  className="w-5 h-5 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                        </svg>
+                    </Button>
+                    <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
+                        <div className="flex flex-col  py-1">
+                            <section  onClick={() => router.push({pathname : '/search' , query : {...query , sort : "mostFavorite"}}) &  handleClose() & setCurrentFilter("محبوب‌ ترین")}>
+                                <p className="py-1.5 px-4 w-full cursor-pointer font-sans text-sm text-center hover:bg-gray-100">محبوب‌ ترین</p>
+                            </section>
 
-                        <section onClick={() => router.push({pathname : '/search' , query : {...query , sort : "dateRecent"}}) & handleClose() & setCurrentFilter("جدید ترین")}>
-                            <p className="py-1.5 px-4 w-full cursor-pointer font-sans text-sm text-center hover:bg-gray-100">جدید ترین</p>
-                        </section>
+                            <section onClick={() => router.push({pathname : '/search' , query : {...query , sort : "dateRecent"}}) & handleClose() & setCurrentFilter("جدید ترین")}>
+                                <p className="py-1.5 px-4 w-full cursor-pointer font-sans text-sm text-center hover:bg-gray-100">جدید ترین</p>
+                            </section>
 
-                        <section onClick={() =>  router.push({pathname : '/search' , query : {...query , sort : "priceMin"}}) & handleClose() & setCurrentFilter("ارزان ترین")}>
-                            <p className="py-1.5 px-4 w-full cursor-pointer font-sans text-sm text-center hover:bg-gray-100">ارزان ترین</p>
-                        </section>
+                            <section onClick={() =>  router.push({pathname : '/search' , query : {...query , sort : "priceMin"}}) & handleClose() & setCurrentFilter("ارزان ترین")}>
+                                <p className="py-1.5 px-4 w-full cursor-pointer font-sans text-sm text-center hover:bg-gray-100">ارزان ترین</p>
+                            </section>
 
-                        <section onClick={() => router.push({pathname : '/search' , query : {...query , sort : "priceMax"}}) & handleClose() & setCurrentFilter("گران ترین")}>
-                            <p className="py-1.5 px-4 w-full cursor-pointer font-sans text-sm text-center hover:bg-gray-100">گران ترین</p>
-                        </section>
-                    </div>
+                            <section onClick={() => router.push({pathname : '/search' , query : {...query , sort : "priceMax"}}) & handleClose() & setCurrentFilter("گران ترین")}>
+                                <p className="py-1.5 px-4 w-full cursor-pointer font-sans text-sm text-center hover:bg-gray-100">گران ترین</p>
+                            </section>
+                        </div>
 
-                </Menu>
+                    </Menu>
+                </div>
             </div>
-        </div>
     );
 }
 export default BreadCrumpAndSort;
