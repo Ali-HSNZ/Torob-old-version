63ecef31d8022c7c3afd7c95c1277a6ee3f11879
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-09-12 20:11:08 +0430

    Create 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,16 @@
+const notFoundPage = () => {
+    return ( 
+        <div className="fixed inset-0 flex justify-center items-center">
+            <div className="px-4">
+                <title>Page Not Found</title>
+                <img src="https://i.ibb.co/W6tgcKQ/softcodeon.gif" className="p-2"/>
+                <h1 className="error-text mt-5 font-sans text-2xl font-bold">صفحه مورد نظر یافت نشد</h1>
+                <p className="text font-sans mt-4  font-bold">لطفا بررسی کنید که آدرس سایت به درستی نوشته شده باشد. </p>
+                <a class="error block font-sans mt-4 font-bold text-red-600" href="/"> به فروشگاه بازگردید </a>
+
+            </div>
+        </div>
+     );
+}
+ 
+export default notFoundPage;
