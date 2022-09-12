ce9722db80bc2d7f705850493facc2f826013b40
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-09-12 20:15:40 +0430

    Use next-Link => src/pages/404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const notFoundPage = () => {
     return ( 
         <div className="fixed inset-0 flex justify-center items-center">
@@ -6,7 +8,9 @@ const notFoundPage = () => {
                 <img src="https://i.ibb.co/W6tgcKQ/softcodeon.gif" className="p-2"/>
                 <h1 className="error-text mt-5 font-sans text-2xl font-bold">صفحه مورد نظر یافت نشد</h1>
                 <p className="text font-sans mt-4  font-bold">لطفا بررسی کنید که آدرس سایت به درستی نوشته شده باشد. </p>
-                <a class="error block font-sans mt-4 font-bold text-red-600" href="/"> به فروشگاه بازگردید </a>
+                <Link href="/">
+                    <a class="error block font-sans mt-4 font-bold text-red-600" > به فروشگاه بازگردید </a>
+                </Link>
 
             </div>
         </div>
