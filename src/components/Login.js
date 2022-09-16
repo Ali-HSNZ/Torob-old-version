f0262f6ec699623df6dad84affbf9d0a256aba4f
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-09-16 23:27:30 +0430

    Change Login Font Color => src/components/Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,9 +6,7 @@ import { useEffect, useState } from "react";
 
 const Login = ({setIsModal , verification_code}) => {
     const dispatch = useAuthDispacher()
-    const {data , loading , error} = useAuth()
-    const [verificationCodeInput , setVerificationCodeInput] = useState('')
-
+    const {data , loading} = useAuth()
     const onSubmit = (values) => {
         const {phoneNumber , validationCode} = values
         if(data){
@@ -37,7 +35,7 @@ const Login = ({setIsModal , verification_code}) => {
     return (  
         <form onSubmit={formik.handleSubmit} className="bg-[#ffffff] rounded-md p-4">
             <a onClick={() => setIsModal(false)} className="flex w-fit justify-end float-left" >
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-gray-800">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                 </svg>
             </a>
@@ -47,24 +45,23 @@ const Login = ({setIsModal , verification_code}) => {
             </div>
             <section className="mt-4 px-10">
                 <div>
-                    <p className="font-sans text-sm font-bold  ">شماره موبایل</p>
-                    <input dir="ltr" name="phoneNumber" onBlur={formik.handleBlur}  value={formik.values['phoneNumber']} onChange={formik.handleChange}  className={` w-full mt-3 font-sans  py-2 bg-gray-50 text-sm p-4  rounded-md border border-gray-300 outline-none`}/>
+                    <p className="font-sans text-sm font-bold text-gray-800 ">شماره موبایل</p>
+                    <input dir="ltr" name="phoneNumber" onBlur={formik.handleBlur}  value={formik.values['phoneNumber']} onChange={formik.handleChange}  className={` w-full mt-3 font-sans  py-2 bg-gray-50 text-sm p-4 text-gray-800 rounded-md border border-gray-300 outline-none`}/>
                 </div>
                     {formik.errors['phoneNumber'] && formik.touched['phoneNumber'] && (
                         <span className={' pt-2 w-full font-sans text-sm text-red-500'}>{formik.errors["phoneNumber"]}</span>
                     )}  
                 <div className={`mt-4 ${data ? "" : "hidden"}`}>
                     <p className="font-sans text-sm font-bold  ">کد تایید</p>
-                    <input dir="ltr" name="validationCode" onBlur={formik.handleBlur}  value={formik.values['validationCode']} onChange={formik.handleChange}  className={` w-full mt-3 font-sans  py-2 bg-gray-50 text-sm p-4  rounded-md border border-gray-300 outline-none`}/>
+                    <input dir="ltr" name="validationCode" onBlur={formik.handleBlur}  value={formik.values['validationCode']} onChange={formik.handleChange}  className={` w-full mt-3 font-sans  py-2 bg-gray-50 text-sm p-4 text-gray-800 rounded-md border border-gray-300 outline-none`}/>
                 </div>
                 
                 <div className="w-full flex justify-center items-center mt-6 ">
                     <button type={'submit'} disabled={!formik.isValid} className="w-10/12 py-1.5 disabled:bg-gray-700 disabled:cursor-not-allowed text-gray-200 rounded-md font-sans bg-red-700 text-sm">
-                        {data && loading ? "ورود به حساب کاربری..." : !data && loading ? "دریافت کد تایید..."  : data ? "ورود به حساب کاربری" : "دریافت کد تایید"}
-                        
+                        {data ? `ورود به حساب کاربری${loading ? "..." : ""}` : `دریافت کد تایید${loading ? "..." : ""}`}
                     </button>
                 </div>
-                <p className="text-xs w-full font-sans mt-4">
+                <p className="text-xs w-full font-sans mt-4 text-gray-800">
                     <span>ثبت نام در ترب به معنی موافقت</span>
                     <span className="text-blue-700"> با شرایط استفاده از ترب</span>
                     <span> است. </span> 
