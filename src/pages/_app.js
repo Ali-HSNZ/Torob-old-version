3c9f3d01fee8863b4cf68e8c5d35309eb3004c30
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-09-16 14:30:32 +0430

    use 'React-Hot-Toast' And Change 'NextNprogress' Options => src/pages/_app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,13 +2,17 @@ import Layout from '../layout/Layout'
 import '../../styles/globals.css'
 import 'tailwindcss/tailwind.css'
 import NextNProgress from "nextjs-progressbar";
+import Auth from 'src/contexts/Auth';
+import { Toaster } from 'react-hot-toast';
 
 function MyApp({ Component, pageProps }) {
   return  (
-    <>
-        <NextNProgress options={{ easing: "ease", speed: 500 }} color="red" />
+    <Auth>
+        <NextNProgress options={{ easing: "ease", speed: 500 , showSpinner : false }} color="#ff3021" />
         <Component {...pageProps} />
-    </>
+        <Toaster position="top-left" toastOptions={{className: '',duration: 5000}}/>
+    </Auth>
+    
   )
 }
 
