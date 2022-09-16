d67a42109c514b697446c952da5e6ab0d2e81734
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-09-16 14:31:33 +0430

    Added FontFamily To 'Sort' Button => src/components/searchPage/main/BreadCrumpAndSort.js

--- src/components/searchPage/main/BreadCrumpAndSort.js
+++ src/components/searchPage/main/BreadCrumpAndSort.js
@@ -90,28 +90,28 @@ const BreadCrumpAndSort = ({category , similarCategories}) => {
                 {/* //? Sort */}
                 <div className="max-w-fit py-2  cursor-pointer hidden  md:flex flex-col justify-end  items-end">
                 
-                    <Button id="basic-button" aria-controls={open ? 'basic-menu' : undefined} aria-haspopup="true" aria-expanded={open ? 'true' : undefined} onClick={handleClick} className="font-sans  text-sm text-gray-600 rounded-md">
+                    <Button sx={{fontFamily : 'iranyekan' , fontSize : "14px", color : '#545353'}} id="basic-button" aria-controls={open ? 'basic-menu' : undefined} aria-haspopup="true" aria-expanded={open ? 'true' : undefined} onClick={handleClick} className="rounded-md">
                         {currentFilter}
-                        <svg  className="w-5 h-5 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                        <svg  className={`w-5 h-5 mr-2 ${anchorEl ? "rotate-180" : ""}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                         </svg>
                     </Button>
                     <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
-                        <div className="flex flex-col  py-1">
+                        <div className="flex flex-col">
                             <section  onClick={() => router.push({pathname : '/search' , query : {...query , sort : "mostFavorite"}}) &  handleClose() & setCurrentFilter("محبوب‌ ترین")}>
-                                <p className="py-1.5 px-4 w-full cursor-pointer text-sm text-center hover:bg-gray-100">محبوب‌ ترین</p>
+                                <p className={`${query.sort && query.sort === "mostFavorite" ? "bg-gray-200" : ""} py-1.5 px-4 w-full font-sans cursor-pointer text-sm text-center hover:bg-gray-100`}>محبوب‌ ترین</p>
                             </section>
 
                             <section onClick={() => router.push({pathname : '/search' , query : {...query , sort : "dateRecent"}}) & handleClose() & setCurrentFilter("جدید ترین")}>
-                                <p className="py-1.5 px-4 w-full cursor-pointer text-sm text-center hover:bg-gray-100">جدید ترین</p>
+                                <p className={`${query.sort && query.sort === "dateRecent" ? "bg-gray-200" : ""} py-1.5 px-4 w-full font-sans cursor-pointer text-sm text-center hover:bg-gray-100`}>جدید ترین</p>
                             </section>
 
                             <section onClick={() =>  router.push({pathname : '/search' , query : {...query , sort : "priceMin"}}) & handleClose() & setCurrentFilter("ارزان ترین")}>
-                                <p className="py-1.5 px-4 w-full cursor-pointer text-sm text-center hover:bg-gray-100">ارزان ترین</p>
+                                <p className={`${query.sort && query.sort === "priceMin" ? "bg-gray-200" : ""} py-1.5 px-4 w-full font-sans cursor-pointer text-sm text-center hover:bg-gray-100`}>ارزان ترین</p>
                             </section>
 
                             <section onClick={() => router.push({pathname : '/search' , query : {...query , sort : "priceMax"}}) & handleClose() & setCurrentFilter("گران ترین")}>
-                                <p className="py-1.5 px-4 w-full cursor-pointer text-sm text-center hover:bg-gray-100">گران ترین</p>
+                                <p className={`${query.sort && query.sort === "priceMax" ? "bg-gray-200" : ""} py-1.5 px-4 w-full font-sans cursor-pointer text-sm text-center hover:bg-gray-100`}>گران ترین</p>
                             </section>
                         </div>
 
