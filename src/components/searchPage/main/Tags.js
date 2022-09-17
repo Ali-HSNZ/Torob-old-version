f63b36dbd59576c9d1f0932e088bcb26994612c4
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-09-18 03:44:49 +0430

    Change SVG Color =>  src/components/searchPage/main/Tags.js

--- src/components/searchPage/main/Tags.js
+++ src/components/searchPage/main/Tags.js
@@ -50,7 +50,7 @@ const MainTags = ({priceMax , priceMin}) => {
                          {query.sort === "dateRecent" && "جدید ترین"}
                          {query.sort === "priceMin" && "ارزان ترین"}
                          {query.sort === "priceMax" && "گران ترین"}
-                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4">
+                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4 text-gray-800">
                              <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                          </svg>
                      </div>
