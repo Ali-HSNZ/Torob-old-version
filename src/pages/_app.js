49b56b3510c2036f38c6fca80c9a1cb0a4729323
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-09-19 04:05:51 +0430

    Use Redux wrapper in _app And Use 'loadUserInfo' on ComponentDidMount => src/pages/_app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,18 +2,26 @@ import Layout from '../layout/Layout'
 import '../../styles/globals.css'
 import 'tailwindcss/tailwind.css'
 import NextNProgress from "nextjs-progressbar";
-import Auth from 'src/contexts/Auth';
 import { Toaster } from 'react-hot-toast';
+import { wrapper } from 'src/redux/store';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { loadUserInfo } from 'src/redux/user/userActions';
 
 function MyApp({ Component, pageProps }) {
+
+  const dispatch = useDispatch()
+  useEffect(()=>{
+      dispatch(loadUserInfo())
+  },[])
+
   return  (
-    <Auth>
+    <>
         <NextNProgress options={{ easing: "ease", speed: 500 , showSpinner : false }} color="#ff3021" />
         <Component {...pageProps} />
-        <Toaster position="top-left" toastOptions={{className: '',duration: 5000}}/>
-    </Auth>
-    
+        <Toaster position="top-left" toastOptions={{className: 'font-sans text-sm',duration: 4000}} />
+    </>
   )
 }
 
-export default MyApp
+export default wrapper.withRedux(MyApp)
