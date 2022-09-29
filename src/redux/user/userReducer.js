d3c7d74ab9c5c1a9511cc3c08bb13423aeea93de
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-09-29 19:09:59 +0330

    Insert Panel in to State / Reafctor Code =>  src/redux/user/userReducer.js

--- src/redux/user/userReducer.js
+++ src/redux/user/userReducer.js
@@ -1,22 +1,17 @@
+const initialState = {user : null , error : null , loading : false , panel : false}
 const { 
     SIGNUP_USER_REQUEST, 
     SIGNUP_USER_SUCCESS, 
-    SIGNUP_USER_FAILURE 
+    SIGNUP_USER_FAILURE,
+    AUTH_PANEL
 } = require("./userTypes");
-
-const initialState = {user : null , error : null , loading : false}
 
 export const userSignupReduser = (state = initialState , action) => {
     switch (action.type) {
-        case SIGNUP_USER_REQUEST:{
-            return {...state , user : null , error : null , loading : true}
-        }
-        case SIGNUP_USER_SUCCESS:{
-            return {...state , user : action.payload , error : null , loading : false}
-        }
-        case SIGNUP_USER_FAILURE:{
-            return {...state , user : null , error : action.payload , loading : false}
-        }
+        case SIGNUP_USER_REQUEST:{return {user : null , error : null , loading : true}}
+        case SIGNUP_USER_SUCCESS:{return {user : action.payload , error : null , loading : false, panel : true}}
+        case SIGNUP_USER_FAILURE:{return {user : null , error : action.payload , loading : false}}
+        case AUTH_PANEL : {return {...state ,panel : action.payload}}
         default: return state
     }
 }
