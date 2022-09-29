c128ac312e4804a0ad2e17302135c1b8229c5235
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-09-29 18:36:51 +0330

    Handle ChartDetail Common with 'page' Props => src/components/productPage/main/Chart.js

--- src/components/productPage/main/Chart.js
+++ src/components/productPage/main/Chart.js
@@ -27,7 +27,7 @@ const Chart = ({chart}) => {
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                                     </svg>
                                 </div>
-                                <ChartDetail chart={chart} />
+                                <ChartDetail chart={chart} page="product"/>
                             </div>
                     </Modal>
             </div>
