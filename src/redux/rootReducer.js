8175a1c689ea7134c2cdfe8f0d4f0249cfa6e910
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-09-29 19:06:37 +0330

    Use 'userSignupReduser' & 'historyReducer' & 'likesReducer' & 'analyticsReducer' => src/redux/rootReducer.js

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,7 +1,13 @@
 import {combineReducers} from 'redux'
+import { historyReducer } from './history/historyReducer'
 import { userSignupReduser } from './user/userReducer'
+import {likesReducer} from './like/likeReducer'
+import { analyticsReducer } from './analytics/analyticsReducer'
 
 const rootReducer = combineReducers({
-    userSignup : userSignupReduser,
+    auth : userSignupReduser,
+    history : historyReducer,
+    likes : likesReducer,
+    analytics : analyticsReducer
 })
 export default rootReducer
