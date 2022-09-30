3b718c2f1a1e8e6780d9475f4e9e88d6414d1ee2
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-10-01 02:08:31 +0330

    Fix Bug / change Category Link / => src/common/BigScreenMenu.js

--- src/common/BigScreenMenu.js
+++ src/common/BigScreenMenu.js
@@ -5,13 +5,13 @@ const BigScreenMenu = ({setUserPanel , customClassname , category  , handleCateg
     const dispatch = useDispatch()
     return (  
         <nav className="flex gap-x-4 ">
-            <button  className="hover:text-red-500  cursor-pointer text-sm font-sans text-gray-500" onClick={()=> handleCategory(category.id) & setUserPanel(false) & dispatch(authPanel(false))}>{category.name}</button>
+            <button  className="hover:text-red-500  cursor-pointer text-sm font-sans text-gray-500" onClick={()=> handleCategory(category.id) & setUserPanel(false) & dispatch(authPanel(false))}>{category.title}</button>
 
             <div className={`${category.status ? "" : "hidden"} ${customClassname}`}>
                 <div className="bg-gray-50 pb-3">
-                    <nav className="px-5 py-4">
-                        <Link href={{pathname : '/search' , query : {category:category.name}}}>
-                            <a className="hover:text-red-500  text-sm font-bold font-sans text-gray-700">{category.name}</a>
+                    <nav className="px-5 py-4"> 
+                        <Link href={{pathname : '/search' , query : {category:category.title}}}>
+                            <a className="hover:text-red-500  text-sm font-bold font-sans text-gray-700">{category.title}</a>
                         </Link>
                     </nav>
                     <hr/>
@@ -20,19 +20,20 @@ const BigScreenMenu = ({setUserPanel , customClassname , category  , handleCateg
                             return(
                                 <div className=" mb-3 break-inside-avoid" key={main_index}>
                                     <nav className="flex gap-x-4">
-                                        <Link href={{pathname : '/search' , query : {category:sub.name}}} >
-                                            <a onClick={()=> closeCategory()} className="hover:text-red-500 font-sans text-sm font-bold cursor-pointer text-gray-800" >{sub.name}</a>
+                                        <Link href={{pathname : '/search' , query : {category:sub.title}}} >
+                                            <a onClick={()=> closeCategory()} className="hover:text-red-500 font-sans text-sm font-bold cursor-pointer text-gray-800" >{sub.title}</a>
                                         </Link>
                                     </nav>
+                                    <nav >
                                     {sub.sub_categories && sub.sub_categories.length > 0 && sub.sub_categories.map((sub_sub,sub_index) => {
+                                        const handlePath = sub_sub.type === 'brand' ? {category : sub_sub.category , brand : sub_sub.brand} : {category : sub_sub.title}
                                         return(
-                                            <nav className="flex gap-x-4 mt-2 " key={sub_index}>
-                                                <Link href={{pathname : '/search' , query : {category:sub_sub.name}}}>
-                                                    <a onClick={()=> closeCategory()} className="hover:text-red-500 text-xs font-sans cursor-pointer text-gray-800" >{sub_sub.name}</a>
+                                                <Link key={sub_index} href={{pathname : '/search' , query : handlePath}}>
+                                                    <a onClick={()=> closeCategory()} className="flex gap-x-4 mt-2 hover:text-red-500 text-xs font-sans cursor-pointer text-gray-800" >{sub_sub.title}</a>
                                                 </Link>
-                                            </nav>
                                         )
                                     })}
+                                    </nav>
                                 </div>
                             )}
                         )}
