32ffa16c10ae6b067dc57c7f158413e8508e6f4d
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-10-09 22:58:13 +0330

    import storeReducer in rootReducer(Redux) => src/redux/rootReducer.js

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,11 +3,12 @@ import { historyReducer } from './history/historyReducer'
 import { userSignupReduser } from './user/userReducer'
 import {likesReducer} from './like/likeReducer'
 import { analyticsReducer } from './analytics/analyticsReducer'
-
+import {storeReducer} from './store/storeReducer'
 const rootReducer = combineReducers({
     auth : userSignupReduser,
     history : historyReducer,
     likes : likesReducer,
-    analytics : analyticsReducer
+    analytics : analyticsReducer,
+    store : storeReducer
 })
 export default rootReducer
