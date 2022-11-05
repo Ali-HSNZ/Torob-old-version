71a7196576bf191ab9d0ebd84a45b97e68145741
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-11-05 21:41:19 +0330

    Wrapping  Layout On NotFound Page => src/pages/404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,19 +1,21 @@
+import Layout from "@/layout/Layout";
 import Link from "next/link";
 
 const notFoundPage = () => {
     return ( 
-        <div className="fixed inset-0 flex justify-center items-center">
-            <div className="px-4">
-                <title>Page Not Found</title>
-                <img src="https://i.ibb.co/W6tgcKQ/softcodeon.gif" className="p-2"/>
-                <h1 className="error-text mt-5 font-sans text-2xl font-bold">صفحه مورد نظر یافت نشد</h1>
-                <p className="text font-sans mt-4  font-bold">لطفا بررسی کنید که آدرس سایت به درستی نوشته شده باشد. </p>
-                <Link href="/">
-                    <a className="error block font-sans mt-4 font-bold text-red-600" > به فروشگاه بازگردید </a>
-                </Link>
-
+        <Layout isFooter={true}>
+            <div className=" flex justify-center items-center mb-8">
+                <div className="px-4">
+                    <title>Page Not Found</title>
+                    <img src="https://i.ibb.co/W6tgcKQ/softcodeon.gif" className="p-2"/>
+                    <h1 className="error-text mt-5 font-sans text-2xl font-bold">صفحه مورد نظر یافت نشد</h1>
+                    <p className="text font-sans mt-4  font-bold">لطفا بررسی کنید که آدرس سایت به درستی نوشته شده باشد. </p>
+                    <Link href="/">
+                        <a className="error block font-sans mt-4 font-bold text-red-600" > به فروشگاه بازگردید </a>
+                    </Link>
+                </div>
             </div>
-        </div>
+        </Layout>
      );
 }
  
