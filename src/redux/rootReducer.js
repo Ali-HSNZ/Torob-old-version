2637016a94eaa673c4eeecd41a02561f1c142f9f
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-11-05 21:53:08 +0330

    import storeReducer - admin_brandsReducer - admin_categoriesReducer - admin_productsReducer => src/redux/rootReducer.js

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,14 +1,22 @@
 import {combineReducers} from 'redux'
+
 import { historyReducer } from './history/historyReducer'
 import { userSignupReduser } from './user/userReducer'
 import {likesReducer} from './like/likeReducer'
 import { analyticsReducer } from './analytics/analyticsReducer'
 import {storeReducer} from './store/storeReducer'
+import {admin_categoriesReducer} from './admin/admin_manageCategory/admin_manageCategoryReducer'
+import { admin_brandsReducer } from './admin/admin_manageBrand/admin_manageBrandReducer'
+import { admin_productsReducer } from './admin/admin_manageProducts/admin_manageProductsReducer'
+
 const rootReducer = combineReducers({
     auth : userSignupReduser,
     history : historyReducer,
     likes : likesReducer,
     analytics : analyticsReducer,
-    store : storeReducer
+    store : storeReducer,
+    admin_brands : admin_brandsReducer,
+    admin_categories : admin_categoriesReducer,
+    admin_products : admin_productsReducer,
 })
 export default rootReducer
