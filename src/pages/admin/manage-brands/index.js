4d1eb34fdea166355896de8c34e814251513a298
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-11-08 11:22:26 +0330

    Moved Form Element Out of data loading - Manage Brands Page =>  src/pages/admin/manage-brands/index.js

--- src/pages/admin/manage-brands/index.js
+++ src/pages/admin/manage-brands/index.js
@@ -104,67 +104,66 @@ const ManageBrands = () => {
                     <Modal open={isAsideModal} onClose={()=>setIsAsideModal(false)} className="lg:hidden">
                         <><AdminPageAside isMobileScreen={true} setIsMobileScreen={setIsAsideModal} mobileScreenClassName={'sm:w-1/2 w-full'}/></>
                     </Modal>
-                        {loading && (
-                            <div className="w-full flex justify-center my-8">
-                                <ReactLoading type="spinningBubbles" height={50} width={50} color="red" />
+
+
+                    <div className="flex justify-between w-full items-center mt-4">
+                        <h1 className="font-sans font-bold text-lg">مدیریت برند</h1>
+                        <div className="flex gap-x-2 items-center">
+                            <Link href={{pathname:"/admin/manage-brands"}}>
+                                <a className="items-center hover:bg-orange-200 bg-orange-100 flex border border-orange-800 text-orange-800 rounded-md py-2  px-3">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+                                    </svg>
+                                </a>
+                            </Link>
+                            <button
+                                    onClick={()=>  {setIsModal_insertBrand(true) & setModalDetail_insertBrand({title:`افرودن برند جدید `})}}
+                                className=" items-center hover:bg-green-200 bg-green-100 flex border border-green-700  rounded-md py-2 px-3"> 
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-green-800">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m6-6H6" />
+                                    </svg>
+                            </button>
+                            <Link href={'/admin'}>
+                                <a className=" items-center hover:bg-blue-200 bg-blue-100 flex border border-[#184e77] text-[#184e77] rounded-md py-2 px-3">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                                    </svg>
+                                </a>
+                            </Link>
+                        </div>
+                    </div>
+                    <form onSubmit={formik.handleSubmit} className="w-full p-4 bg-white mt-3 rounded-lg shadow-md">
+                        <section className="mt-3 grid grid-cols-3 gap-4">
+                            <div className="flex flex-col relative">
+                                <p className="font-sans text-sm"> عنوان برند :</p>
+                                <input type={"text"} value={formik.values.name} name="name" onBlur={formik.handleBlur} onChange={formik.handleChange}  placeholder="عنوان برند را وارد کنید" className="border-gray-300 hover:border-gray-600  focus:border-gray-600 focus:ring-0 text-sm mt-2 font-sans bg-white text-gray-800 rounded-md "/>
+                                {formik.errors.name && formik.touched.name && <p className="mt-2 font-sans text-xs text-red-700">{formik.errors.name}</p>}
                             </div>
-                        )}
-                    {!loading && (
-                        <>
-                            <div className="flex justify-between w-full items-center mt-4">
-                                <h1 className="font-sans font-bold text-lg">مدیریت برند</h1>
-                                <div className="flex gap-x-2 items-center">
-                                    <Link href={{pathname:"/admin/manage-brands"}}>
-                                        <a className="items-center hover:bg-orange-200 bg-orange-100 flex border border-orange-800 text-orange-800 rounded-md py-2  px-3">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-                                                <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
-                                            </svg>
-                                        </a>
-                                    </Link>
-                                    <button
-                                         onClick={()=>  {setIsModal_insertBrand(true) & setModalDetail_insertBrand({title:`افرودن برند جدید `})}}
-                                        className=" items-center hover:bg-green-200 bg-green-100 flex border border-green-700  rounded-md py-2 px-3"> 
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-green-800">
-                                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m6-6H6" />
-                                            </svg>
-                                    </button>
-                                    <Link href={'/admin'}>
-                                        <a className=" items-center hover:bg-blue-200 bg-blue-100 flex border border-[#184e77] text-[#184e77] rounded-md py-2 px-3">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-                                                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-                                            </svg>
-                                        </a>
-                                    </Link>
-                                </div>
+                            <div className="flex flex-col relative">
+                                <p className="font-sans text-sm">نام شرکت :</p>
+                                <input type={"text"} name="company" value={formik.values.company} onBlur={formik.handleBlur} onChange={formik.handleChange} placeholder="نام شرکت را وارد کنید" className="border-gray-300 hover:border-gray-600  focus:border-gray-600 focus:ring-0 text-sm mt-2 font-sans bg-white text-gray-800 rounded-md "/>
+                                {formik.errors.company && formik.touched.company && <p className="mt-2 font-sans text-xs text-red-700">{formik.errors.company}</p>}
                             </div>
-                            <form onSubmit={formik.handleSubmit} className="w-full p-4 bg-white mt-3 rounded-lg shadow-md">
-                                <section className="mt-3 grid grid-cols-3 gap-4">
-                                    <div className="flex flex-col relative">
-                                        <p className="font-sans text-sm"> عنوان برند :</p>
-                                        <input type={"text"} value={formik.values.name} name="name" onBlur={formik.handleBlur} onChange={formik.handleChange}  placeholder="عنوان برند را وارد کنید" className="border-gray-300 hover:border-gray-600  focus:border-gray-600 focus:ring-0 text-sm mt-2 font-sans bg-white text-gray-800 rounded-md "/>
-                                        {formik.errors.name && formik.touched.name && <p className="mt-2 font-sans text-xs text-red-700">{formik.errors.name}</p>}
-                                    </div>
-                                    <div className="flex flex-col relative">
-                                        <p className="font-sans text-sm">نام شرکت :</p>
-                                        <input type={"text"} name="company" value={formik.values.company} onBlur={formik.handleBlur} onChange={formik.handleChange} placeholder="نام شرکت را وارد کنید" className="border-gray-300 hover:border-gray-600  focus:border-gray-600 focus:ring-0 text-sm mt-2 font-sans bg-white text-gray-800 rounded-md "/>
-                                        {formik.errors.company && formik.touched.company && <p className="mt-2 font-sans text-xs text-red-700">{formik.errors.company}</p>}
-                                    </div>
-                                    <div className="flex flex-col relative">
-                                        <p className="font-sans text-sm">وضعیت :</p>
-                                        <select defaultValue={ router.query.state || 'all'} onChange={event => setStatus(event.target.value)} className=" cursor-pointer border-gray-300 hover:border-gray-600  focus:border-gray-600 focus:ring-0 text-sm mt-2 font-sans bg-white text-gray-800 rounded-md">
-                                            <option className="py-2 text-sm font-sans" value={'active'}>تایید شده‌ها</option>
-                                            <option className="py-2 text-sm font-sans" value={'trashed'}>رد شده‌ها</option>
-                                            <option className="py-2 text-sm font-sans" value={'all'} >نمایش همه وضعیت ها</option>
-                                        </select>
-                                    </div>
-                                </section>
-        
-                                <section className="w-full flex justify-end mt-3 items-center">
-                                    <button type={"submit"} className={`${formik.isValid ? "hover:bg-blue-200 bg-blue-100 border border-blue-600 text-blue-800 cursor-pointer " : "cursor-not-allowed hover:bg-gray-800 bg-gray-700 border border-gray-600 text-gray-100"}  py-[6px] px-6 font-sans  text-sm rounded-md`}>جستجو</button>
-                                </section>
-                            </form>
-                        </>
+                            <div className="flex flex-col relative">
+                                <p className="font-sans text-sm">وضعیت :</p>
+                                <select defaultValue={ router.query.state || 'all'} onChange={event => setStatus(event.target.value)} className=" cursor-pointer border-gray-300 hover:border-gray-600  focus:border-gray-600 focus:ring-0 text-sm mt-2 font-sans bg-white text-gray-800 rounded-md">
+                                    <option className="py-2 text-sm font-sans" value={'active'}>تایید شده‌ها</option>
+                                    <option className="py-2 text-sm font-sans" value={'trashed'}>رد شده‌ها</option>
+                                    <option className="py-2 text-sm font-sans" value={'all'} >نمایش همه وضعیت ها</option>
+                                </select>
+                            </div>
+                        </section>
+
+                        <section className="w-full flex justify-end mt-3 items-center">
+                            <button type={"submit"} className={`${formik.isValid ? "hover:bg-blue-200 bg-blue-100 border border-blue-600 text-blue-800 cursor-pointer " : "cursor-not-allowed hover:bg-gray-800 bg-gray-700 border border-gray-600 text-gray-100"}  py-[6px] px-6 font-sans  text-sm rounded-md`}>جستجو</button>
+                        </section>
+                    </form>
+                    {loading && (
+                        <div className="w-full flex justify-center my-8">
+                            <ReactLoading type="spinningBubbles" height={50} width={50} color="red" />
+                        </div>
                     )}
+
                     {!brands && !loading  &&(
                         <Warning text={"برندی یافت نشد! می توانید برند جدیدی ثبت کنید."}></Warning>
                     )}
