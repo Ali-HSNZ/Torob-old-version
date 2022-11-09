aa794d73ce5d1ce4e4336bf311135493eb74a5f3
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-11-10 00:09:37 +0330

    Change 'Edit Product Page' Validation  => src/pages/admin/manage-products/edit/[productId].js

--- src/pages/admin/manage-products/edit/[productId].js
+++ src/pages/admin/manage-products/edit/[productId].js
@@ -58,7 +58,7 @@ const EditProduct = () => {
 
     const validationSchema = Yup.object({
         product_title : Yup.string().min(10, "نام کالا نمی‌تواند کم تر از ۱۰ نویسه باشد").max(250 , 'نام کالا نمی تواند بیشتر از ۲۵۰ نویسه باشد').trim().required("نام کالا نمی تواند خالی باشد"),
-        product_description : Yup.string().min(20,"توضیحات کالا نمیتواند کم تر از ۲۰ نویسه باشد").max(500,"توضیحات کالا نمی تواند بیشتر از ۵۰۰ نویسه باشد").required("توضیحات کالا نمی تواند خالی باشد").trim(),
+        product_description : Yup.string().min(20,"توضیحات کالا نمیتواند کم تر از ۲۰ نویسه باشد").trim().required("توضیحات کالا نمی تواند خالی باشد"),
         barcode : Yup.string().length(12,"بارکد باید ۱۲ رقم باشد").required("مقدار بارکد نمی تواند خالی باشد").matches(/^[0-9]{12}\d*$/,"مقدار بارکد باید عدد باشد").trim()
     })
     const [onChangeFile , setOnChangeFile] = useState(null)
