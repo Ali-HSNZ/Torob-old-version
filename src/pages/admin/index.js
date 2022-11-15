a6b1867a25d8cb1a54e8126096c629ede10a8235
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-11-15 23:03:40 +0330

    Set '/admin/manage-stores' Link Address in Admin Page  => src/pages/admin/index.js

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -8,14 +8,12 @@ import { HiOutlineDocumentText } from 'react-icons/hi';
 import { BiCategory } from 'react-icons/bi';
 import { TbBrandAsana } from 'react-icons/tb';
 import Link from "next/link";
-import { useSelector } from "react-redux";
 import Cookies from "universal-cookie";
 import axios from "axios";
 
 const AdminPage = () => {
     const [isAsideModal , setIsAsideModal] = useState(false)
 
-    const {user} = useSelector(state => state.auth)
     return (  
         <Layout isFooter={true}>
             <div className="w-full flex flex-col lg:flex-row  justify-between ">
@@ -29,9 +27,8 @@ const AdminPage = () => {
                     </Modal>
                     <section className="w-full ">
 
-                        {/* //! Card */}
                         <div className="mt-4 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
-                        {/* //! User Card */}
+
                             <div className="cursor-pointer py-4 hover:bg-orange-50 flex items-center justify-between rounded-xl bg-white shadow-lg overflow-hidden">
                                 <div className="flex h-12 mr-4 w-[67px] items-center justify-center rounded-full border border-orange-200 bg-orange-50">
                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-orange-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
@@ -108,21 +105,22 @@ const AdminPage = () => {
                                 </a>
                             </Link>
 
-
-                            <div className="cursor-pointer py-4 hover:bg-red-50 flex items-center justify-between rounded-xl bg-white shadow-lg overflow-hidden">
-                                <div className="flex h-12 mr-4 w-[67px]  items-center justify-center rounded-full border border-red-200 bg-red-50">
-                                    <svg   className="h-6 w-6 text-red-700" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 21v-7.5a.75.75 0 01.75-.75h3a.75.75 0 01.75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349m-16.5 11.65V9.35m0 0a3.001 3.001 0 003.75-.615A2.993 2.993 0 009.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 002.25 1.016c.896 0 1.7-.393 2.25-1.016a3.001 3.001 0 003.75.614m-16.5 0a3.004 3.004 0 01-.621-4.72L4.318 3.44A1.5 1.5 0 015.378 3h13.243a1.5 1.5 0 011.06.44l1.19 1.189a3 3 0 01-.621 4.72m-13.5 8.65h3.75a.75.75 0 00.75-.75V13.5a.75.75 0 00-.75-.75H6.75a.75.75 0 00-.75.75v3.75c0 .415.336.75.75.75z" />
-                                    </svg>
-                                </div>
-                                <div className="mr-4 w-full">
-                                    <h2 className="font-sans font-bold">{toPersianDigits(574)} فروشگاه</h2>
-                                    <span className="font-sans text-xs text-gray-500">ویرایش | حذف | جستجو</span>
-                                </div>
-                                <div className="h-full flex items-center ml-1">
-                                    <BsFillCaretLeftFill className="text-red-400 "/>
-                                </div>
-                            </div>
+                            <Link href={'/admin/manage-stores'}>
+                                <a className="cursor-pointer py-4 hover:bg-red-50 flex items-center justify-between rounded-xl bg-white shadow-lg overflow-hidden">
+                                    <div className="flex h-12 mr-4 w-[67px]  items-center justify-center rounded-full border border-red-200 bg-red-50">
+                                        <svg   className="h-6 w-6 text-red-700" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 21v-7.5a.75.75 0 01.75-.75h3a.75.75 0 01.75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349m-16.5 11.65V9.35m0 0a3.001 3.001 0 003.75-.615A2.993 2.993 0 009.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 002.25 1.016c.896 0 1.7-.393 2.25-1.016a3.001 3.001 0 003.75.614m-16.5 0a3.004 3.004 0 01-.621-4.72L4.318 3.44A1.5 1.5 0 015.378 3h13.243a1.5 1.5 0 011.06.44l1.19 1.189a3 3 0 01-.621 4.72m-13.5 8.65h3.75a.75.75 0 00.75-.75V13.5a.75.75 0 00-.75-.75H6.75a.75.75 0 00-.75.75v3.75c0 .415.336.75.75.75z" />
+                                        </svg>
+                                    </div>
+                                    <div className="mr-4 w-full">
+                                        <h2 className="font-sans font-bold">{toPersianDigits(574)} فروشگاه</h2>
+                                        <span className="font-sans text-xs text-gray-500">ویرایش | حذف | جستجو</span>
+                                    </div>
+                                    <div className="h-full flex items-center ml-1">
+                                        <BsFillCaretLeftFill className="text-red-400 "/>
+                                    </div>
+                                </a>
+                            </Link>
 
                         </div>
                     </section>
@@ -142,7 +140,7 @@ export const getServerSideProps = async(ctx) => {
     await axios.get("https://market-api.iran.liara.run/api/user", {headers : {Authorization : `Bearer ${token}`}})
     .then(({data}) =>  {
         if(data.user.account_type !== 'admin') ErrorCode = 403
-    })
+    })  
     .catch( () => ErrorCode = 403)
     if(ErrorCode === 403){
         return{notFound : true}
