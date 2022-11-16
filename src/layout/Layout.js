3f5590ce798b48e4db24cd99eda5fab682abc0d4
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-11-16 16:25:28 +0330

    Set 'Head/next' For Set Title And ... In page => src/layout/Layout.js

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,11 +1,15 @@
+import Head from "next/head";
 import Footer from "./Footer";
 import Header from "./Header";
 
 
 
-const Layout = ({children , isFooter}) => {
+const Layout = ({children , isFooter,pageTitle}) => {
     return (  
         <div>
+            <Head>
+                <title>{pageTitle || "عنوان  صفحه نامشخص"}</title>
+            </Head>
             <Header/>
             {children}
            {isFooter ? <Footer/> : <></>}
