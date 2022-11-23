5291e35e317ca2e4231b3bcc4506232439e4bc37
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-11-23 15:01:41 +0330

    Refactor Code And Show Server Error in Auth Actions => src/redux/user/userActions.js

--- src/redux/user/userActions.js
+++ src/redux/user/userActions.js
@@ -1,71 +1,63 @@
 import axios from "axios" 
-import { useRouter } from "next/router"
 import toast from "react-hot-toast"
 import Cookies from "universal-cookie"
 import { 
-    //? SignUp Types
-    SIGNUP_USER_FAILURE, 
-    SIGNUP_USER_REQUEST, 
-    SIGNUP_USER_SUCCESS ,
-    //? SignIn Types
-    SIGNIN_USER_FAILURE, 
-    SIGNIN_USER_REQUEST, 
-    SIGNIN_USER_SUCCESS ,
-    //? Authentication Panel
+    AUTH_FAILURE, 
+    AUTH_REQUEST, 
+    AUTH_SUCCESS,
     AUTH_PANEL
-    
 } from "./userTypes"
 
-//? SignUp Actions
-const signupUserRequest = () => { return {type : SIGNUP_USER_REQUEST}}
-const signupUserSuccess = (user) => { return {type : SIGNUP_USER_SUCCESS , payload : user}}
-const signupUserFailure = (error) => {return {type : SIGNUP_USER_FAILURE , payload : error}}
-//? SignIn Actions
-const signinUserRequest = () => { return {type : SIGNIN_USER_REQUEST}}
-const signinUserSuccess = (user) => { return {type : SIGNIN_USER_SUCCESS , payload : user}}
-const signinUserFailure = (error) => {return {type : SIGNIN_USER_FAILURE , payload : error}}
+// Auth Actions
+const authRequest = () => { return {type : AUTH_REQUEST}}
+const authSuccess = (payload) => { return {type : AUTH_SUCCESS , payload}}
+const authFailure = (payload) => {return {type : AUTH_FAILURE , payload}}
 
-
+// Sign Up User With Phone Number
 export const userSignup = (phone_number) => {
     return (dispatch) => {
-        dispatch(signupUserRequest())
+        dispatch(authRequest())
         axios.post("https://market-api.iran.liara.run/api/login" , {phone_number})
         .then(response => {
-            dispatch(signupUserSuccess(response.data))
+            dispatch(authSuccess(response.data))
             toast.success("کد احراز هویت : "+response.data.verification_code)
         }).catch(err => {
-            dispatch(signupUserFailure(err.response?.data?.message ? err.response.data.message : "خطای سرور در بخش احراز هویت" ))
-            toast.error(err.response?.data?.message ? err.response.data.message : "خطای سرور در بخش احراز هویت" )
+            const message = err.response?.data?.message || " user Signup - خطای سرور در بخش احراز هویت";
+            toast.error(message)
+            dispatch(authFailure(message))
         })
     }
 }
-
+// Sign in User With Phone Number & Code
 export const userSignin = (data) => {
     return (dispatch) => {
-        dispatch(signinUserRequest())
+        dispatch(authRequest())
         axios.post("https://market-api.iran.liara.run/api/verify" ,data)
         .then(response => {
             window.location.reload()
-            dispatch(signinUserSuccess(response.data))
+            dispatch(authSuccess(response.data))
             new Cookies().set('userToken' ,response.data.API_TOKEN,{path:'/'} )
             toast.success(" با موفقیت وارد حساب کاربری خود شدید")
         })
         .catch(err => {
-            dispatch(signinUserFailure(err.response.data.message))
-            toast.error(err.response.data.message)
+            const message = err.response?.data?.message || "خطای سرور در بخش احراز هویت";
+            toast.error(message)
+            dispatch(authFailure(message))
         })
     }
 }
-//? Load User Data When Site Refreshed
+// Load User Data When Site Refreshed
 export const loadUserInfo = () => {
     return (dispatch) => {
-        dispatch(signinUserRequest())
+        dispatch(authRequest())
         const token = new Cookies().get("userToken");
         axios.get("https://market-api.iran.liara.run/api/user", {headers : {Authorization : `Bearer ${token}`}})
         .then(response => {
-            dispatch(signinUserSuccess(response.data.user))
+            dispatch(authSuccess(response.data.user))
         }).catch(err => {
-            dispatch(signinUserFailure(err.response?.data?.message ? err.response.data.message : "خطای سرور در بخش احراز هویت" ))
+            const message = err.response?.data?.message || "خطای سرور در بخش احراز هویت";
+            toast.error(message)
+            dispatch(authFailure(message))
         })
     }
 }
@@ -75,4 +67,4 @@ export const userLogout = () => dispatch =>  {
     window.location.reload()
 }
 
-export const authPanel = (payload) => (dispatch) => dispatch({type : AUTH_PANEL , payload })
+export const authPanel = (payload) => dispatch => dispatch({type : AUTH_PANEL , payload })
