2e8eb2eb13538b4239601a3290cdf7d70c10f03b
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-11-23 15:04:48 +0330

    Replace Auth Types in 'AuthReducer' - For Example : 'SIGNUP_USER_REQUEST' To 'AUTH_REQUEST' => src/redux/user/userReducer.js

--- src/redux/user/userReducer.js
+++ src/redux/user/userReducer.js
@@ -1,16 +1,17 @@
 const initialState = {user : null , error : null , loading : false , panel : false}
 const { 
-    SIGNUP_USER_REQUEST, 
-    SIGNUP_USER_SUCCESS, 
-    SIGNUP_USER_FAILURE,
+    AUTH_REQUEST, 
+    AUTH_SUCCESS, 
+    AUTH_FAILURE,
     AUTH_PANEL
 } = require("./userTypes");
 
-export const userSignupReduser = (state = initialState , action) => {
+export const authReduser = (state = initialState , action) => {
     switch (action.type) {
-        case SIGNUP_USER_REQUEST:{return {user : null , error : null , loading : true}}
-        case SIGNUP_USER_SUCCESS:{return {user : action.payload , error : null , loading : false, panel : true}}
-        case SIGNUP_USER_FAILURE:{return {user : null , error : action.payload , loading : false}}
+        case AUTH_REQUEST:{return {user : state.user , error : null , loading : true,panel : state.panel}}
+        case AUTH_SUCCESS:{return {user : action.payload , error : null , loading : false, panel : true}}
+        case AUTH_FAILURE:{return {user : null , error : action.payload , loading : false}}
+        
         case AUTH_PANEL : {return {...state ,panel : action.payload}}
         default: return state
     }
