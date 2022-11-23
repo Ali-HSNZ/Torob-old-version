9b33a30a2c24c59df8b2b7e09d11fac4e127de65
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-11-23 19:36:18 +0330

    Create 'CHANGE_PANEL_TYPE' case in userReducer => src/redux/user/userReducer.js

--- src/redux/user/userReducer.js
+++ src/redux/user/userReducer.js
@@ -1,18 +1,23 @@
-const initialState = {user : null , error : null , loading : false , panel : false}
+const initialState = {user : null , error : null , loading : false , panel : false , panelType:'normal'}
 const { 
     AUTH_REQUEST, 
     AUTH_SUCCESS, 
     AUTH_FAILURE,
-    AUTH_PANEL
+    AUTH_PANEL,
+    CHANGE_PANEL_TYPE
 } = require("./userTypes");
 
 export const authReduser = (state = initialState , action) => {
     switch (action.type) {
-        case AUTH_REQUEST:{return {user : state.user , error : null , loading : true,panel : state.panel}}
-        case AUTH_SUCCESS:{return {user : action.payload , error : null , loading : false, panel : true}}
-        case AUTH_FAILURE:{return {user : null , error : action.payload , loading : false}}
+        case AUTH_REQUEST:{return {...state , user : state.user , error : null , loading : true,panel : state.panel}}
+        case AUTH_SUCCESS:{return {...state , user : action.payload , error : null , loading : false, panel : true}}
+        case AUTH_FAILURE:{return {...state , user : null , error : action.payload , loading : false}}
         
-        case AUTH_PANEL : {return {...state ,panel : action.payload}}
+        case AUTH_PANEL : {
+            const {isOpen,type} = action.payload
+            return {...state , panelType : type,panel : isOpen}
+        }
+        case CHANGE_PANEL_TYPE : {return {...state ,panelType : action.payload}}
         default: return state
     }
 }
