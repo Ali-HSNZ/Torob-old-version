ad299389cc9837f23071bc8e9d7ebefb838f83ea
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-11-25 02:33:35 +0330

    import 'manageStore_reducer' in rootReducer (Redux) =>  src/redux/rootReducer.js

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -10,6 +10,7 @@ import { admin_brandsReducer } from './admin/admin_manageBrand/admin_manageBrand
 import { admin_productsReducer } from './admin/admin_manageProducts/admin_manageProductsReducer'
 import { admin_storesReducer } from './admin/admin_manageStores/admin_manageStoresReducer'
 import { admin_manageUsersReducer } from './admin/admin_manageUsers/admin_manageUsersReducer'
+import { manageStoreReducer } from './manage-store/manageStore_reducer'
 
 const rootReducer = combineReducers({
     auth : authReduser,
@@ -17,10 +18,15 @@ const rootReducer = combineReducers({
     likes : likesReducer,
     analytics : analyticsReducer,
     store : storeReducer,
+
+    // Admin Panel Reducers
     admin_brands : admin_brandsReducer,
     admin_categories : admin_categoriesReducer,
     admin_products : admin_productsReducer,
     admin_stores : admin_storesReducer,
-    admin_users : admin_manageUsersReducer
+    admin_users : admin_manageUsersReducer,
+    
+    // Store Panel Reducers 
+    manage_store : manageStoreReducer,
 })
 export default rootReducer
