fb62b37061ef62efc297ad457f03f7d313404dcf
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-11-27 01:20:21 +0330

    Create And Use 'FetchStoreCount' Types in 'ManageStore' Reducer (Redux) => src/redux/manage-store/insertProduct/manageStore_reducer.js

--- src/redux/manage-store/insertProduct/manageStore_reducer.js
+++ src/redux/manage-store/insertProduct/manageStore_reducer.js
@@ -15,16 +15,27 @@ const {
     STORE_INSERT_PRODUCT_SUCCESS,
     STORE_INSERT_PRODUCT_FAILURE,
 
+    FETCH_STORE_COUNT_REQUEST, 
+    FETCH_STORE_COUNT_SUCCESS, 
+    FETCH_STORE_COUNT_FAILURE, 
+
 } = require("./manageStore_types")
 
 const initialState = {
     products:{ products : null,loading : false,error : null},
     categories:{categories : null,loading : false,error : null},
     brands:{brands : null,loading : false,error : null},
+    mainDataCount:{data : null,loading : false,error : null},
 }
 export const manageStoreReducer = (state = initialState , action) => {
     switch (action.type) {
-        // Fetch Admin Products
+        // Fetch Store Data Count => Show in Main Panel 
+
+        case FETCH_STORE_COUNT_REQUEST: {return {...state , mainDataCount : {data : null , loading : true , error : null}}}
+        case FETCH_STORE_COUNT_SUCCESS: {return {...state , mainDataCount : {data : action.payload , loading : false , error : null}}}
+        case FETCH_STORE_COUNT_FAILURE: {return {...state , mainDataCount : {data : null , loading : false , error : action.payload}}}
+        
+        // Fetch Store Products
         case STORE_FETCH_PRODUCTS_BASES_REQUEST : {
             return {...state,products : {products : null , loading : true , error : null}}
         }
@@ -42,7 +53,7 @@ export const manageStoreReducer = (state = initialState , action) => {
             return {...state,products : {products : null , loading : false , error : action.payload}}
         }
 
-        // Fetch Admin Brands
+        // Fetch Store Brands
         case STORE_FETCH_BRANDS_REQUEST : {
             return {...state,brands : {brands : null, loading : true,error : null}}
         }
@@ -53,7 +64,7 @@ export const manageStoreReducer = (state = initialState , action) => {
             return {...state,brands : {brands : null, loading : false,error : action.payload}}
         }
 
-        // Fetch Admin Categories
+        // Fetch Store Categories
         case STORE_FETCH_CATEGORIES_REQUEST : { return { ...state, categories:{ categories : null , loading : true, error : null,}}}
         case STORE_FETCH_CATEGORIES_SUCCESS : { return { ...state, categories : { categories : action.payload.categories, loading : false, error : null}}}
         case STORE_FETCH_CATEGORIES_FAILURE : {return { ...state, categories : { categories : null, loading : false, error : action.payload}}}
