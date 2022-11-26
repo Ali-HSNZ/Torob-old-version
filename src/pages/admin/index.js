224d936b16536938feb0b6d2f5afdb83055876ac
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-11-26 13:37:51 +0330

    Change Main Admin Page Styles in Admin Panel => src/pages/admin/index.js

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -27,10 +27,10 @@ const AdminPage = () => {
                     </Modal>
                     <section className="w-full ">
 
-                        <div className="mt-4 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
+                        <div className="mt-4 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 pb-4">
 
                             <Link  href={'/admin/manage-users'}>
-                                <a className="cursor-pointer py-4 hover:bg-orange-50 flex items-center justify-between rounded-xl bg-white shadow-lg overflow-hidden">
+                                <a className="cursor-pointer py-4 hover:bg-orange-50 flex items-center justify-between rounded-xl bg-white shadow-md overflow-hidden">
                                     <div className="flex h-12 mr-4 w-[67px] items-center justify-center rounded-full border border-orange-200 bg-orange-50">
                                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-orange-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
@@ -47,7 +47,7 @@ const AdminPage = () => {
                             </Link>
 
                             <Link href={'/admin/manage-products'}>
-                                <a className="cursor-pointer py-4 hover:bg-blue-50 flex items-center justify-between rounded-xl bg-white shadow-lg overflow-hidden">
+                                <a className="cursor-pointer py-4 hover:bg-blue-50 flex items-center justify-between rounded-xl bg-white shadow-md overflow-hidden">
                                     <div className="flex h-12 mr-4 w-[67px]  items-center justify-center rounded-full border border-blue-200 bg-blue-50">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-6 w-6 text-blue-700">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9" />
@@ -65,7 +65,7 @@ const AdminPage = () => {
 
 
                             <Link href={'/admin/manage-brands'}>
-                                <a className="cursor-pointer py-4 hover:bg-pink-50 flex items-center justify-between rounded-xl bg-white shadow-lg overflow-hidden">
+                                <a className="cursor-pointer py-4 hover:bg-pink-50 flex items-center justify-between rounded-xl bg-white shadow-md overflow-hidden">
                                     <div className="flex h-12 mr-4 w-[67px]  items-center justify-center rounded-full border border-pink-200 bg-pink-50">
                                         <TbBrandAsana className="h-6 w-6 text-pink-700"/>
                                     </div>
@@ -80,7 +80,7 @@ const AdminPage = () => {
                             </Link>
 
                             <Link href={'/admin/manage-category'}>
-                                <a className="cursor-pointer py-4 hover:bg-purple-50 flex items-center justify-between rounded-xl bg-white shadow-lg overflow-hidden">
+                                <a className="cursor-pointer py-4 hover:bg-purple-50 flex items-center justify-between rounded-xl bg-white shadow-md overflow-hidden">
                                     <div className="flex h-12 mr-4 w-[67px]  items-center justify-center rounded-full border border-purple-200 bg-purple-50">
                                         <BiCategory className="h-6 w-6 text-purple-700"/>
                                     </div>
@@ -95,7 +95,7 @@ const AdminPage = () => {
                             </Link>
 
                             <Link href={'/admin/manage-stores'}>
-                                <a className="cursor-pointer py-4 hover:bg-red-50 flex items-center justify-between rounded-xl bg-white shadow-lg overflow-hidden">
+                                <a className="cursor-pointer py-4 hover:bg-red-50 flex items-center justify-between rounded-xl bg-white shadow-md overflow-hidden">
                                     <div className="flex h-12 mr-4 w-[67px]  items-center justify-center rounded-full border border-red-200 bg-red-50">
                                         <svg   className="h-6 w-6 text-red-700" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 21v-7.5a.75.75 0 01.75-.75h3a.75.75 0 01.75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349m-16.5 11.65V9.35m0 0a3.001 3.001 0 003.75-.615A2.993 2.993 0 009.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 002.25 1.016c.896 0 1.7-.393 2.25-1.016a3.001 3.001 0 003.75.614m-16.5 0a3.004 3.004 0 01-.621-4.72L4.318 3.44A1.5 1.5 0 015.378 3h13.243a1.5 1.5 0 011.06.44l1.19 1.189a3 3 0 01-.621 4.72m-13.5 8.65h3.75a.75.75 0 00.75-.75V13.5a.75.75 0 00-.75-.75H6.75a.75.75 0 00-.75.75v3.75c0 .415.336.75.75.75z" />
