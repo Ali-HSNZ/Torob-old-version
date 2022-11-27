3e67e427cd51eb4183a4d658240275dd7a0e7166
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-11-27 23:29:48 +0330

    Change 'PHONE_NUMBER_REGIX' And Create 'POSTAL_CODE_REGIX' Regex in Utils File => src/utils/Regex.js

--- src/utils/Regex.js
+++ src/utils/Regex.js
@@ -1,2 +1,3 @@
 export const ONLY_DIGIT_REGIX = /^\d+$/;
 export const PHONE_NUMBER_REGIX = /^(?:98|\+98|0098|0)?9[0-9]{9}$/
+export const POSTAL_CODE_REGIX = /\b(?!(\d)\1{3})[13-9]{4}[1346-9][013-9]{5}\b/;
