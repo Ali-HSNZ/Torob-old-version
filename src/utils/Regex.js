9ffb3176af86ae70b74e24fb44289a847481dd4b
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-12-06 20:02:21 +0330

    Create 'PASSWORD_REGIX' Regix in Regix Utils =>  src/utils/Regex.js

--- src/utils/Regex.js
+++ src/utils/Regex.js
@@ -1,3 +1,4 @@
 export const ONLY_DIGIT_REGIX = /^\d+$/;
 export const PHONE_NUMBER_REGIX = /^(?:98|\+98|0098|0)?9[0-9]{9}$/
 export const POSTAL_CODE_REGIX = /\b(?!(\d)\1{3})[13-9]{4}[1346-9][013-9]{5}\b/;
+export const PASSWORD_REGIX = /^.{6,12}$/
