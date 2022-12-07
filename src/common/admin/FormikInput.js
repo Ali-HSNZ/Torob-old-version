934c9269f2aea541bc589448dc37a2b8d4b70da4
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-12-07 18:24:59 +0330

    Change Styles in 'FormikInput' => src/common/admin/FormikInput.js

--- src/common/admin/FormikInput.js
+++ src/common/admin/FormikInput.js
@@ -1,18 +1,18 @@
 import { setComma } from "@/utils/setComma";
+import { TextField } from "@mui/material";
 
 const FormikInput = ({isComma , formik,name,placeholder,title,parentClassName,isRequired}) => {
     return (  
         <div className={parentClassName}>
-            <section className="w-auto flex flex-col items-right gap-x-1 pb-0">
-                <p className={`font-sans text-sm text-gray-800 ${isRequired ? "before:content-['*'] before:text-red-600" : ""}`}>{title} :</p>
+            <section className="w-auto flex flex-col items-right gap-x-1">
+                <p className={`font-sans text-[13px] text-gray-800 ${isRequired ? "before:content-['*'] before:text-red-600" : ""}`}>{title} :</p>
                 <input 
                     type="text" 
-                    name={`${name}`} 
-                    placeholder={placeholder}
+                    name={name} 
                     onChange={formik.handleChange} 
                     value={isComma ? setComma(formik.values[name]) : formik.values[name]} 
                     onBlur={formik.handleBlur} 
-                    className="mt-2 w-full border-gray-300 hover:border-gray-600  focus:border-gray-600 focus:ring-0 text-sm  font-sans bg-white text-gray-800 rounded-md "
+                    className={`${formik.errors[name] && formik.touched[name] ? "border-red-400 hover:border-red-600  focus:border-red-600" : "border-gray-300 hover:border-gray-600  focus:border-gray-600"} mt-2 w-full  focus:ring-0 text-sm  font-sans bg-white text-gray-800 rounded-md`}
                 />
             </section>
             {formik.errors[name] && formik.touched[name] && <p className={'text-red-600 font-sans text-xs pt-2'}>{formik.errors[name]}</p>}
