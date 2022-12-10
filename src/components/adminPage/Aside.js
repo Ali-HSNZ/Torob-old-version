903ac0c421926266a68a7ae12ce6189ea4e11805
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-12-10 20:23:51 +0330

    Create 'change-password' link in Admin Aside Menu => src/components/adminPage/Aside.js

--- src/components/adminPage/Aside.js
+++ src/components/adminPage/Aside.js
@@ -72,6 +72,15 @@ const AdminPageAside = ({isMobileScreen , setIsMobileScreen  , mobileScreenClass
                         <span className="font-sans mr-2 font-bold text-sm text-gray-900">مدیریت فروشگاه‌ها</span>
                     </a>
                 </Link>
+
+                <Link href={'/admin/change-password'}>
+                    <a className={` ${router.asPath.startsWith('/admin/change-password') ? "bg-red-200 hover:bg-red-300" : "hover:bg-gray-100"} flex items-center pr-8 py-3 hover:bg-gray-100`}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6  text-gray-900">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 5.25a3 3 0 013 3m3 0a6 6 0 01-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1121.75 8.25z" />
+                        </svg>
+                        <span className="font-sans mr-2 font-bold text-sm text-gray-900">تغییر رمز عبور</span>
+                    </a>
+                </Link>
             </div>
 
         </aside>
