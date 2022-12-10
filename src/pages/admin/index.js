b6d7163d52077402fc33c54d11db6890c12052a1
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-12-10 20:21:41 +0330

    Create 'change-password' Link in Admin Main Page =>  src/pages/admin/index.js

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -115,6 +115,25 @@ const AdminPage = () => {
                                 </a>
                             </Link>
 
+                            <Link href={'/admin/change-password'}>
+                                <a className="cursor-pointer py-4 hover:bg-yellow-50 flex items-center justify-between rounded-xl bg-white shadow-lg overflow-hidden">
+                                    <div className="flex h-12 mr-4 w-[67px]  items-center justify-center rounded-full border border-yellow-200 bg-yellow-50">
+                                        <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6  text-yellow-600">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 5.25a3 3 0 013 3m3 0a6 6 0 01-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1121.75 8.25z" />
+                                        </svg>
+                                    </div>
+                                    <div className="mr-4 w-full">
+                                        <h2 className="font-sans font-bold"> تغییر رمز عبور</h2>
+                                        <span className="font-sans text-xs text-gray-500">ویرایش</span>
+                                    </div>
+                                    <div className="h-full flex items-center ml-1">
+                                        <BsFillCaretLeftFill className="text-yellow-400 "/>
+                                    </div>
+                                </a>
+                            </Link>
+
+                            
+
                         </div>
                     </section>
                 </section>
