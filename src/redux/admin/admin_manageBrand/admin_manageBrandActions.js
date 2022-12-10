ecb5670f3993fee7e84c3943a65a5cd46de14cda
Ali_Hassanzadeh <Aliatraby@gmail.com>
2022-12-10 11:53:44 +0330

    Refactor manageBrand Actions (Redux) => src/redux/admin/admin_manageBrand/admin_manageBrandActions.js

--- src/redux/admin/admin_manageBrand/admin_manageBrandActions.js
+++ src/redux/admin/admin_manageBrand/admin_manageBrandActions.js
@@ -20,56 +20,66 @@ export const fetchBrands = (payload) => dispatch => {
     dispatch(admin_fetchBrandsRequest())
     axios.get(`https://market-api.iran.liara.run/api/admin/brands?state=${state || 'all'}&order=${order || 'desc'}&name=${paramsName || ""}&company=${paramsCompany || ""}&page=${page || 1}&limit=${limit || 12}` , {headers : {authorization : `Bearer ${token}`}})
     .then(({data}) => dispatch(admin_fetchBrandsSuccess(data)))
-    .catch(error => dispatch(admin_fetchBrandsFailure(error.response.data.message || "خطای سرور در بخش  گرفتن لیست دسته بندی")))
+    .catch(error => {
+        const message = 'خطای سرور در بخش گرفتن لیست برندها'
+        const serverMessage_list = error?.response?.data?.errors
+        if(serverMessage_list && serverMessage_list.length > 0) serverMessage_list.forEach(error => toast.error(error));
+        if(!serverMessage_list) toast.error(message)
+        dispatch(admin_fetchBrandsFailure(message))
+    })
 }
 
 export const deleteBrand = ({state ,order, page , limit,paramsName,paramsCompany,id}) => dispatch => {
-    const payload = {state ,order, page , limit,paramsName,paramsCompany}
     dispatch(admin_fetchBrandsRequest())
-    axios.put(`https://market-api.iran.liara.run/api/admin/brands/${id}/state` ,{}, {headers : {authorization : `Bearer ${token}`}})
-    .then(() => dispatch(fetchBrands(payload)))
-    .catch(error => dispatch(admin_fetchBrandsFailure(error.response.data.message || "خطای سرور در بخش  گرفتن لیست دسته بندی")))
+    axios.put(`https://market-api.iran.liara.run/api/admin/brands/${id}/state?state=${state || 'all'}&order=${order || 'desc'}&name=${paramsName || ""}&company=${paramsCompany || ""}&page=${page || 1}&limit=${limit || 12}` ,{}, {headers : {authorization : `Bearer ${token}`}})
+    .then(({data}) => dispatch(admin_fetchBrandsSuccess(data)))
+    .catch(error => {
+        const message = 'خطای سرور در بخش حذف برند'
+        const serverMessage_list = error?.response?.data?.errors
+        if(serverMessage_list && serverMessage_list.length > 0) serverMessage_list.forEach(error => toast.error(error));
+        if(!serverMessage_list) toast.error(message)
+        dispatch(admin_fetchBrandsFailure(message))
+    })
 }
 
 export const updateBrand = ({brandImage , order, companyName , faName ,enName  ,state , page , limit,paramsName,paramsCompany,id}) => dispatch => {
+    const brand_logo = brandImage.selectedFile ? brandImage.selectedFile : brandImage.imageUrl ? 0 : 1;
     const slug = enName.replace(/\s+/g, '-')
-    const payload = {state , page ,order, limit,paramsName,paramsCompany}
     dispatch(admin_fetchBrandsRequest())
-    axios.post(`https://market-api.iran.liara.run/api/admin/brands/${id}/update`, {
+    axios.post(`https://market-api.iran.liara.run/api/admin/brands/${id}/update?state=${state || 'all'}&order=${order || 'desc'}&name=${paramsName || ""}&company=${paramsCompany || ""}&page=${page || 1}&limit=${limit || 12}`, {
         brand_name : faName,
         brand_english_name : enName,
         brand_company : companyName,
         slug,
-        brand_logo : brandImage || 1
+        brand_logo : brand_logo
     } , {headers : {'content-type' : 'multipart/form-data' ,authorization : `Bearer ${token}`,}})
-    .then((data) =>{
-        dispatch(fetchBrands(payload))
-        const message = data.data.error
-        if(message) toast.error(message);
-        else toast.success('تغییرات با موفقیت ثبت شد')
+    .then(({data}) =>{
+        dispatch(admin_fetchBrandsSuccess(data))
+        if(data.status === 401) toast.error(data.message);
     })
     .catch(error => {
+        const message = 'خطای سرور در بخش ویرایش برند'
         const serverMessage_list = error?.response?.data?.errors
         if(serverMessage_list && serverMessage_list.length > 0) serverMessage_list.forEach(error => toast.error(error));
-        else dispatch(admin_fetchBrandsFailure( "خطا در ثبت تغییرات"))
+        if(!serverMessage_list) toast.error(message)
+        dispatch(admin_fetchBrandsFailure(message))
     })
 }
 
 export const insertBrand = (payload) => dispatch => {
     const slug = payload.enName.replace(/\s+/g, '-')
-    const {faName : brand_name, enName : brand_english_name, companyName : brand_company, brandImage, page, limit,order } = payload;
+    const {paramsName,paramsCompany,state,faName : brand_name, enName : brand_english_name, companyName : brand_company, brandImage, page, limit,order } = payload;
     dispatch(admin_fetchBrandsRequest())
-    axios.post(`https://market-api.iran.liara.run/api/admin/brands?page=${page}&limit=${limit}`, {brand_name, brand_english_name, brand_company, slug, brand_logo : brandImage || null } , {headers : {'content-type' : 'multipart/form-data' , authorization : `Bearer ${token}`}})
-    .then((data) =>{
-        dispatch(fetchBrands(payload))
-        const message = data.data.error
-        if(message) toast.error(message);
-        else toast.success('برند با موفقیت ثبت شد')
+    axios.post(`https://market-api.iran.liara.run/api/admin/brands?page=${page}&limit=${limit}state=${state || 'all'}&order=${order || 'desc'}&name=${paramsName || ""}&company=${paramsCompany || ""}&page=${page || 1}&limit=${limit || 12}`, {brand_name, brand_english_name, brand_company, slug, brand_logo : brandImage || null } , {headers : {'content-type' : 'multipart/form-data' , authorization : `Bearer ${token}`}})
+    .then(({data}) =>{
+        dispatch(admin_fetchBrandsSuccess(data))
+        if(data.status === 401) toast.error(data.message);
     })
     .catch(error => {
-        dispatch(fetchBrands(payload))
+        const message = 'خطای سرور در بخش ثبت برند'
         const serverMessage_list = error?.response?.data?.errors
         if(serverMessage_list && serverMessage_list.length > 0) serverMessage_list.forEach(error => toast.error(error));
-        else dispatch(admin_fetchBrandsFailure( "خطا در ثبت برند"))
+        if(!serverMessage_list) toast.error(message)
+        dispatch(admin_fetchBrandsFailure(message))
     })
 }
