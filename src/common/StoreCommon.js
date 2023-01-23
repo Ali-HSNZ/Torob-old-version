2c2b0544713d70887c0cd05df0e17a24fac2a3d7
Ali_Hassanzadeh <Aliatraby@gmail.com>
2023-01-23 17:48:53 +0330

    Change Store Common Button Styles in Product Page => src/common/StoreCommon.js

--- src/common/StoreCommon.js
+++ src/common/StoreCommon.js
@@ -43,38 +43,38 @@ const StoreCommon = ({store , index}) => {
                               <span className="whitespace-nowrap font-sans text-sm mr-1">تخفیف پله‌ایی</span>
                          </label>}
                     </section>
-                    <section className="flex flex-col sm:flex-row items-center gap-x-5 justify-between    sm:mr-4">
-                         <p className={`text-red-600   font-bold whitespace-nowrap font-sans mb-4 sm:m-0`}>{toPersianPrice(store.price) +" تومان " }</p>
+                    <section className="flex flex-col-reverse  sm:flex-row items-center gap-x-5 justify-between    sm:mr-4">
+                         <p className={`text-red-600   font-bold whitespace-nowrap font-sans `}>{toPersianPrice(store.price) +" تومان " }</p>
                          {availableStoreInCart({store_id : store.store_id}) ? (
-                         <div className="flex bg-white  border-2 border-red-500 rounded-md items-center">
-                              {/* increase */}
-                              <button onClick={()=>dispatch(increaseProductToCart({product_id : store.product_id , store_id : store.store_id}))} className="font-sans text-sm p-2">
-                                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-gray-800">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                                   </svg>
-                              </button>
-                              {isIncreaseProductLoading({store_id : store.store_id}) ? (
-                                   <ReactLoading type="spinningBubbles" height={20} width={20} color="red" />
-                              ) : (
-                                   <span className="font-sans text-sm px-1">{toPersianDigits(availableStoreInCart({store_id : store.store_id}).count)}</span>
-                              )}
-                              {/* Decrease */}
-                              <button onClick={()=>dispatch(decreaseProductToCart({product_id : store.product_id , store_id : store.store_id}))} className="font-sans text-sm p-2">
-                                   {availableStoreInCart({store_id : store.store_id}).count <= 1 ? (
-                                        // Trash Icon
+                              <div className="flex bg-white  border-2 border-red-500 rounded-md items-center mb-4 sm:m-0">
+                                   {/* increase */}
+                                   <button onClick={()=>dispatch(increaseProductToCart({product_id : store.product_id , store_id : store.store_id}))} className="font-sans text-sm p-2">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-gray-800">
-                                             <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                             <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                                         </svg>
+                                   </button>
+                                   {isIncreaseProductLoading({store_id : store.store_id}) ? (
+                                        <ReactLoading type="spinningBubbles" height={20} width={20} color="red" />
                                    ) : (
-                                        // Decrease Icon
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-gray-800">
-                                             <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12h-15" />
-                                        </svg>
+                                        <span className="font-sans text-sm px-1">{toPersianDigits(availableStoreInCart({store_id : store.store_id}).count)}</span>
                                    )}
-                              </button>
-                         </div> 
+                                   {/* Decrease */}
+                                   <button onClick={()=>dispatch(decreaseProductToCart({product_id : store.product_id , store_id : store.store_id}))} className="font-sans text-sm p-2">
+                                        {availableStoreInCart({store_id : store.store_id}).count <= 1 ? (
+                                             // Trash Icon
+                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-gray-800">
+                                                  <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                             </svg>
+                                        ) : (
+                                             // Decrease Icon
+                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-gray-800">
+                                                  <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12h-15" />
+                                             </svg>
+                                        )}
+                                   </button>
+                              </div> 
                          ) : (
-                         <button onClick={()=> user ? dispatch(addProductToCart({product_id : store.product_id , store_id : store.store_id})) : dispatch(authPanel({type : 'userPath' , isOpen : true}))} className={` whitespace-nowrap  bg-red-600 border-red-600  py-1.5 text-white   border  font-sans rounded-md font-bold  text-sm px-4 `}>افزودن به سبد</button>
+                              <button onClick={()=> user ? dispatch(addProductToCart({product_id : store.product_id , store_id : store.store_id})) : dispatch(authPanel({type : 'userPath' , isOpen : true}))} className={`mb-4 sm:m-0 whitespace-nowrap  bg-red-600 border-red-600  py-1.5 text-white   border  font-sans rounded-md font-bold  text-sm px-4 `}>افزودن به سبد</button>
                          )}
                     </section>
                </section>
