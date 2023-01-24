15bb8b25f593a7926ceaacf6a40b4416c0e8b0fd
Ali_Hassanzadeh <Aliatraby@gmail.com>
2023-01-24 21:11:31 +0330

    Refactor Code And Removed Unused Code in _app => src/pages/_app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,15 +4,9 @@ import NextNProgress from "nextjs-progressbar";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { wrapper } from 'src/redux/store';
-// import { useEffect } from 'react';
-// import { useDispatch } from 'react-redux';
-// import { loadUserInfo } from 'src/redux/user/userActions';
-// import '@etchteam/next-pagination/dist/index.css'
-// import { fetchCartItems } from '@/redux/cart/cart/cartActions';
-// import { token } from 'src/services/http';
-
 
 function MyApp({ Component, pageProps }) {
+     
      return  (
           <>
                <NextNProgress options={{ easing: "ease", speed: 500 , showSpinner : false }} color="#ff3021" />
