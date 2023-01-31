c44060a27c2a34e8de0e7cd43857a629c12c636d
Ali_Hassanzadeh <Aliatraby@gmail.com>
2023-01-31 15:34:03 +0330

    Create Home Link And 'Manage-Factors' Link in Store Aside (Menu) => src/components/manageStore/storeAside.js

--- src/components/manageStore/storeAside.js
+++ src/components/manageStore/storeAside.js
@@ -4,7 +4,6 @@ import { useRouter } from "next/router";
 const ManageStoreAside = ({isMobileScreen , setIsMobileScreen  , mobileScreenClassName }) => {
 
      const router = useRouter()
-
      return (  
           <aside className={`${mobileScreenClassName ? mobileScreenClassName : "w-1/5"}   h-screen bg-white ${isMobileScreen ? "lg:hidden" : "hidden"} sticky top-0 lg:flex flex-col overflow-y-auto`}>
                
@@ -23,6 +22,16 @@ const ManageStoreAside = ({isMobileScreen , setIsMobileScreen  , mobileScreenCla
                )}
 
                <nav className="py-3">
+
+                    <Link href={'/store'}>
+                         <a  className={` ${router.pathname === '/store' ? "bg-red-200 hover:bg-red-300" : "hover:bg-gray-100"} flex items-center pr-8 py-3 hover:bg-gray-100`}>
+                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-gray-900">
+                                   <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                              </svg>
+                              <span className="font-sans mr-2 font-bold text-sm text-gray-900">داشبورد</span>
+                         </a>
+                    </Link>
+
                     <Link href={'/store/manage-products'}>
                          <a  className={` ${router.asPath.startsWith('/store/manage-products') ? "bg-red-200 hover:bg-red-300" : "hover:bg-gray-100"} flex items-center pr-8 py-3 hover:bg-gray-100`}>
                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-6 w-6 text-gray-900">
@@ -33,14 +42,14 @@ const ManageStoreAside = ({isMobileScreen , setIsMobileScreen  , mobileScreenCla
                     </Link>
                     
 
-                    {/* <Link href={'#'}>
-                         <a className={` ${router.asPath.startsWith('/store/manage-order') ? "bg-red-200 hover:bg-red-300" : "hover:bg-gray-100"} flex items-center pr-8 py-3 hover:bg-gray-100`}>
-                         <svg className="h-6 w-6 text-gray-900" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-                              <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 21v-7.5a.75.75 0 01.75-.75h3a.75.75 0 01.75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349m-16.5 11.65V9.35m0 0a3.001 3.001 0 003.75-.615A2.993 2.993 0 009.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 002.25 1.016c.896 0 1.7-.393 2.25-1.016a3.001 3.001 0 003.75.614m-16.5 0a3.004 3.004 0 01-.621-4.72L4.318 3.44A1.5 1.5 0 015.378 3h13.243a1.5 1.5 0 011.06.44l1.19 1.189a3 3 0 01-.621 4.72m-13.5 8.65h3.75a.75.75 0 00.75-.75V13.5a.75.75 0 00-.75-.75H6.75a.75.75 0 00-.75.75v3.75c0 .415.336.75.75.75z" />
-                         </svg>
-                         <span className="font-sans mr-2 font-bold text-sm text-gray-900">مدیریت سفارشات</span>
+                    <Link href={'/store/manage-factors'}>
+                         <a className={` ${router.asPath.startsWith('/store/manage-factors') ? "bg-red-200 hover:bg-red-300" : "hover:bg-gray-100"} flex items-center pr-8 py-3 hover:bg-gray-100`}>
+                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-gray-900">
+                                   <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+                              </svg>
+                              <span className="font-sans mr-2 font-bold text-sm text-gray-900">مدیریت سفارشات</span>
                          </a>
-                    </Link> */}
+                    </Link>
 
                     <Link href={'/store/change-password'}>
                          <a className={` ${router.asPath.startsWith('/store/change-password') ? "bg-red-200 hover:bg-red-300" : "hover:bg-gray-100"} flex items-center pr-8 py-3 hover:bg-gray-100`}>
