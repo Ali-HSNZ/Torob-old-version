41dc20f85a27487c7edf0fca5608bece0fe87f4d
Ali_Hassanzadeh <Aliatraby@gmail.com>
2023-01-31 15:35:48 +0330

    Create Home Link And Refresh Page Link in Manage Factors Page in Admin Panel =>  src/pages/store/manage-factors/index.js

--- src/pages/store/manage-factors/index.js
+++ src/pages/store/manage-factors/index.js
@@ -111,8 +111,16 @@ const ManageFactors = () => {
                                    </button>
                                    <h1 className="font-sans font-bold text-lg">مدیریت فاکتور</h1>
                               </div>
-                              <nav className="flex gap-x-2 items-center">
-                                   {/* Home SVG */}
+                              <nav className="flex gap-x-2 my-2 sm:m-0 justify-end  sm:items-center">
+                                   {/* Refresh */}
+                                   <Link href={{pathname:"/store/manage-factors"}}>
+                                        <a className="items-center hover:bg-red-200 bg-red-100 flex border border-red-800 text-red-800 rounded-md py-2  px-3">
+                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                                                  <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+                                             </svg>
+                                        </a>
+                                   </Link>
+                                   {/* Home */}
                                    <Link href={'/store'}>
                                         <a className=" items-center hover:bg-blue-200 bg-blue-100 flex border border-[#184e77] text-[#184e77] rounded-md py-2 px-3">
                                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
