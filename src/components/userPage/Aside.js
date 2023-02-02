11f3dc609a00fb3d3176bb206a625aad53d53eaa
Ali_Hassanzadeh <Aliatraby@gmail.com>
2023-02-02 17:55:11 +0330

    Create 'invoices' Page Link in UserPage Aside (Menu) => src/components/userPage/Aside.js

--- src/components/userPage/Aside.js
+++ src/components/userPage/Aside.js
@@ -51,14 +51,19 @@ const UserPageAside = ({isMobileScreen,setIsMobileScreen,mobileScreenClassName})
                               <span className="font-sans mr-2  font-bold text-sm">مشاهدات اخیر</span>
                          </a>
                     </Link>
+                    <Link href={"/user/invoices"}>
+                         <a className={` flex items-center pr-8 py-3 ${router.pathname === "/user/invoices" ? "bg-red-50 text-red-600 ": "hover:bg-gray-50 "}`}>
+                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                   <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+                              </svg>
+                              <span className="font-sans mr-2  font-bold text-sm">سفارشات</span>
+                         </a>
+                    </Link>
                </nav>
                <hr />
 
                <nav className="flex py-3 flex-col">
-                    <Link href={"#"}>
-                         <a className="text-gray-700 font-sans pr-8 text-sm py-3 hover:bg-gray-50">لیست فروشگاه‌های ترب</a>
-                    </Link>
-                    <Link href={"#"}>
+                    <Link href={"/signup/store"}>
                          <a className="text-gray-700 font-sans pr-8 text-sm py-3 hover:bg-gray-50">ثبت نام فروشگاه</a>
                     </Link>
                </nav>
