a0dd5c96656a60c05c37848b1787053082f884f7
Ali_Hassanzadeh <Aliatraby@gmail.com>
2023-02-06 01:03:07 +0330

    Change 'buttonClassName' And 'linkClassName' in global utils => src/utils/global.js

--- src/utils/global.js
+++ src/utils/global.js
@@ -19,15 +19,15 @@ export const checkImageFormat = ({fileName}) => {
 
 export const buttonClassName = ({ bgColor , isValid , isOutline}) => {
      if(isOutline){
-          return `shadow-${bgColor}-300 focus:ring-2  ring-green-600 ring-offset-2 shadow-sm md:shadow-md  lg:shadow-lg ${isValid ? ` hover:bg-${bgColor}-200 bg-${bgColor}-100 border border-${bgColor}-600 text-${bgColor}-800 cursor-pointer` : "ring-gray-400 cursor-not-allowed hover:bg-gray-800 bg-gray-700 border border-gray-600 text-gray-100"}  py-2 px-6 font-sans  text-sm rounded-md`
+          return `shadow-${bgColor}-300 shadow-sm md:shadow-md  lg:shadow-lg ${isValid ? ` hover:bg-${bgColor}-200 bg-${bgColor}-100 border border-${bgColor}-600 text-${bgColor}-800 cursor-pointer` : "ring-gray-400 cursor-not-allowed hover:bg-gray-800 bg-gray-700 border border-gray-600 text-gray-100"}  py-2 px-6 font-sans  text-sm rounded-md`
      }else{
-          return `focus:ring-2 shadow-${bgColor}-400  ring-offset-2 shadow-sm md:shadow-md  lg:shadow-lg ${isValid ? `ring-${bgColor}-400 hover:bg-${bgColor}-700 bg-${bgColor}-600 border   text-white cursor-pointer ` : "ring-gray-400 cursor-not-allowed hover:bg-gray-800 bg-gray-700 border border-gray-600 text-gray-100"}  py-2 px-6 font-sans  text-sm rounded-md`
+          return ` shadow-${bgColor}-400  shadow-sm md:shadow-md  lg:shadow-lg ${isValid ? `ring-${bgColor}-400 hover:bg-${bgColor}-700 bg-${bgColor}-600 border   text-white cursor-pointer ` : "ring-gray-400 cursor-not-allowed hover:bg-gray-800 bg-gray-700 border border-gray-600 text-gray-100"}  py-2 px-6 font-sans  text-sm rounded-md`
      }
 }
 
 export const linkClassName =  ({ bgColor , isOutline}) => {
      if(isOutline) {
-          return `shadow-${bgColor}-300 focus:ring-2  ring-offset-2 shadow-sm md:shadow-md  lg:shadow-lg ring-${bgColor}-400 hover:bg-${bgColor}-200 bg-${bgColor}-100 border border-${bgColor}-600 text-${bgColor}-800 cursor-pointer py-2 px-6 font-sans  text-sm rounded-md`
+          return `shadow-${bgColor}-300  focus:ring-2  ring-offset-2 shadow-sm md:shadow-md  lg:shadow-lg ring-${bgColor}-400 hover:bg-${bgColor}-200 bg-${bgColor}-100 border border-${bgColor}-600 text-${bgColor}-800 cursor-pointer py-2 px-6 font-sans  text-sm rounded-md`
      }else{
           return `focus:ring-2 shadow-${bgColor}-400 ring-offset-2 shadow-sm md:shadow-md  lg:shadow-lg  ring-${bgColor}-600 hover:bg-${bgColor}-700 bg-${bgColor}-600 border   text-white cursor-pointer  py-2 px-6 font-sans  text-sm rounded-md`
      }
