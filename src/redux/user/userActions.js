7bdbc19e6a9e8770ba9c2b86b968729fe58b9823
Ali_Hassanzadeh <Aliatraby@gmail.com>
2023-02-06 16:04:58 +0330

    Refactor user Actions (Redux) Requests  =>  src/redux/user/userActions.js

--- src/redux/user/userActions.js
+++ src/redux/user/userActions.js
@@ -13,16 +13,13 @@ export const authRequest = () => { return {type : AUTH_REQUEST}}
 export const authSuccess = (payload) => { return {type : AUTH_SUCCESS , payload}}
 export const authFailure = (payload) => {return {type : AUTH_FAILURE , payload}}
 
-// Sign Up User With Phone Number
-// export const userSignup = (phone_number) =>  dispatch => {
-
 export const userSignin = (data) => dispatch => {
      dispatch(authRequest())
-     http.post("https://market-api.iran.liara.run/api/login/credentials" ,data)
+     http.post("login/credentials" ,data)
      .then(({data}) => {
           dispatch(authSuccess(data.user))
           setCookies({key : "userToken" , value : data.API_TOKEN , config : {path:'/'} })
-          requestSuccess(" با موفقیت وارد حساب کاربری خود شدید")
+          requestSuccess(" با موفقیت وارد حساب کاربری خود شدید.")
           if(data.is_password === false){
                setTimeout(() => window.location.href = '/store/change-password', 1000);
           }else{
@@ -42,10 +39,7 @@ export const loadUserInfo = () => dispatch => {
      .then(({data}) => {
           dispatch(authSuccess(data.user))
           dispatch(cartDetails(data))
-     }).catch(error => {
-          // requestError({error : error?.response?.data?.errors , defaultMessage : "خطا در بخش احراز هویت"})
-          dispatch(authFailure("خطا در بخش احراز هویت"))
-     })
+     }).catch(error => dispatch(authFailure("خطا در بخش احراز هویت")))
 }
 
 export const userLogout = () => () =>  {
@@ -58,4 +52,5 @@ export const userLogout = () => () =>  {
 export const authPanel = (payload) => dispatch => dispatch({type : AUTH_PANEL , payload })
 
 // Example => type is Normal Or userPass
+// Example => dispatch(changePanelType('userPass'))
 export const changePanelType = (payload) => dispatch => dispatch({type : CHANGE_PANEL_TYPE , payload })
