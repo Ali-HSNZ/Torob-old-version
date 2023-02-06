7fe5965f4855c5650ebf8c71b2b7435b11b3ade0
Ali_Hassanzadeh <Aliatraby@gmail.com>
2023-02-06 15:33:09 +0330

    Change User Button And User Panel in Header Component =>  src/components/Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -53,9 +53,10 @@ const Header = () => {
 
                {/* User Panel */}
                <section className="w-full sm:w-fit flex justify-end  ">
+                    {/* Cart */}
                     <Link href={'/cart'}>
-                         <a className="relative flex items-center justify-center ml-4 bg-white px-4 py-2 border border-gray-300 rounded-md text-xs font-sans text-gray-500"> 
-                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 ml-1">
+                         <a className="relative flex items-center justify-center ml-4 bg-white px-4 py-2 border border-gray-300 rounded-md text-xs font-sans text-gray-700"> 
+                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 ml-1">
                                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                               </svg>
                               سبد خرید
@@ -63,75 +64,75 @@ const Header = () => {
 
                          </a>
                     </Link>
-                    {user && user.account_type === 'normal' ? (
-                         <>
-                              <button onClick={() => handleUserPanel_btn()} className="userPhoneNumber_btn bg-white px-6 py-2 border border-gray-300 rounded-md text-xs font-sans text-gray-500 min-w-[121px] max-w-[121px]" >
-                                   {toPersianDigits(user.phone_number_primary)}
-                              </button>
-                              <nav className={`bg-gray-50 rounded-b-md userPanel hidden absolute  top-[37px] left-0  whitespace-nowrap `} >
-                                   <Link href={'/user/favorites'} >
-                                        <a className="text-xs cursor-pointer hover:bg-gray-200 px-[22px] font-bold text-gray-700 py-2 text-center font-sans block">محبوب‌ها</a>
-                                   </Link>
-                                   <Link href={'/user/history'} >
-                                        <a className="text-xs cursor-pointer hover:bg-gray-200 px-[22px] font-bold text-gray-700 py-2 text-center font-sans block">مشاهدات اخیر</a>
-                                   </Link>
-                                   <Link href={'/user/invoices'} >
-                                        <a className="text-xs cursor-pointer hover:bg-gray-200 px-[22px] font-bold text-gray-700 py-2 text-center font-sans block">سفارشات</a>
-                                   </Link>
-                                   <button onClick={() => { dispatch(userLogout())}} className="text-xs cursor-pointer hover:bg-red-100 px-6 font-bold text-red-600 w-full py-2 text-center font-sans ">
-                                        خروج
-                                   </button>
-                              </nav>
-                         </>
-                    ) : user ? (
-                         <>
-                              <button onClick={() => handleUserPanel_btn()} className="userPhoneNumber_btn bg-white px-6 py-2 border border-gray-300 rounded-md text-xs font-sans text-gray-500 min-w-[121px] max-w-[121px]" >
-                                   {toPersianDigits(user.phone_number_primary)}
-                              </button>
-                              <div className={`bg-white rounded-b-md   border overflow-hidden border-gray-300 border-t-0 userPanel hidden absolute  top-[32px] left-[0px]  whitespace-nowrap `}>
-                                   {user.is_pending ? (
-                                        <>
-                                             <button onClick={()=>requestError({error : null , defaultMessage : ' فروشگاه شما در وضعیت "بررسی نشده" است. و پس از بررسی به پنل خود دسترسی خواهید داشت'})} className="text-xs min-w-[120px] max-w-[120px] cursor-pointer hover:bg-gray-200 font-bold text-gray-700 py-2 text-center font-sans block">پنل مدیریت</button>
-                                             <nav>
-                                                  <Link href={'/user/favorites'} >
-                                                       <a className="text-xs cursor-pointer hover:bg-gray-200 px-[22px] font-bold text-gray-700 py-2 text-center font-sans block">محبوب‌ها</a>
-                                                  </Link>
-                                                  <Link href={'/user/history'} >
-                                                       <a className="text-xs cursor-pointer hover:bg-gray-200 px-[22px] font-bold text-gray-700 py-2 text-center font-sans block">مشاهدات اخیر</a>
-                                                  </Link>
-                                                  <Link href={'/user/invoices'} >
-                                                       <a className="text-xs cursor-pointer hover:bg-gray-200 px-[22px] font-bold text-gray-700 py-2 text-center font-sans block">سفارشات</a>
-                                                  </Link>
-                                             </nav>
-                                        </>
-                                   ) : (
-                                        <nav>
-                                             <Link href={`/${user.account_type}`} >
-                                                  <a className="text-xs cursor-pointer min-w-[120px] max-w-[120px] hover:bg-gray-200 font-bold text-gray-700 py-2 text-center font-sans block">پنل مدیریت</a>
-                                             </Link>
-                                             <Link href={'/user/favorites'} >
-                                                  <a className="text-xs cursor-pointer hover:bg-gray-200 px-[22px] font-bold text-gray-700 py-2 text-center font-sans block">محبوب‌ها</a>
-                                             </Link>
-                                             <Link href={'/user/history'} >
-                                                  <a className="text-xs cursor-pointer hover:bg-gray-200 px-[22px] font-bold text-gray-700 py-2 text-center font-sans block">مشاهدات اخیر</a>
-                                             </Link>
-                                             <Link href={'/user/invoices'} >
-                                                  <a className="text-xs cursor-pointer hover:bg-gray-200 px-[22px] font-bold text-gray-700 py-2 text-center font-sans block">سفارشات</a>
-                                             </Link>
-                                        </nav>
-                                   )}
-                                   <button onClick={()=> {dispatch(userLogout())}} className="min-w-[120px] max-w-[120px] text-xs cursor-pointer hover:bg-red-100  font-bold text-red-600 w-full py-2 text-center font-sans ">خروج</button>
-                              </div>
-                         </>
-                    ) : (
-                         <>
-                              {loading ? (
-                                   <button className="cursor-default bg-white px-4 py-2 border border-gray-300 rounded-md text-xs font-sans text-gray-500">...</button>
-                              ) : ( 
-                                   <button onClick={() => dispatch(authPanel({isOpen : true,type : "normal"}))} className="bg-white px-4 py-2 border border-gray-300 rounded-md text-xs font-sans text-gray-500">ورود / ثبت نام</button>
-                              )}
-                         </>
-                    )}
+                    <section className="relative w-[123px]">
+
+                        {user?.phone_number_primary && <button onClick={() => handleUserPanel_btn()} className="w-full userPhoneNumber_btn bg-white  py-2 border border-gray-300 rounded-md text-xs font-sans text-gray-700 " >
+                            {toPersianDigits(user?.phone_number_primary)}
+                        </button>}
+
+                        {user ? (
+                            <>
+                                {user?.account_type === 'normal' ? (
+                                    <nav className={`bg-white rounded-b-md border overflow-hidden border-gray-300 border-t-0 userPanel hidden absolute w-full top-[30px] left-[0px]  whitespace-nowrap `} >
+                                        <Link href={'/user/favorites'} >
+                                            <a className="text-xs cursor-pointer hover:bg-gray-200 font-bold text-gray-700 py-2 text-center font-sans block">محبوب‌ها</a>
+                                        </Link>
+                                        <Link href={'/user/history'} >
+                                            <a className="text-xs cursor-pointer hover:bg-gray-200 font-bold text-gray-700 py-2 text-center font-sans block">مشاهدات اخیر</a>
+                                        </Link>
+                                        <Link href={'/user/invoices'} >
+                                            <a className="text-xs cursor-pointer hover:bg-gray-200 font-bold text-gray-700 py-2 text-center font-sans block">سفارشات</a>
+                                        </Link>
+                                        <button onClick={() => {dispatch(userLogout())}} className="text-xs cursor-pointer  hover:bg-red-100 px-6 font-bold text-red-700 w-full py-2 text-center font-sans ">
+                                            خروج
+                                        </button>
+                                    </nav>
+                                ) : (
+                                    <div className={`bg-white rounded-b-md   border overflow-hidden border-gray-300 border-t-0 userPanel hidden absolute  top-[30px] w-full left-[0px]  whitespace-nowrap `}>
+                                        {user?.is_pending ? (
+                                            <nav className="w-full">
+                                                <button onClick={()=>requestError({error : null , defaultMessage : ' فروشگاه شما در وضعیت "بررسی نشده" است. و پس از بررسی به پنل خود دسترسی خواهید داشت.'})} className="text-xs w-full cursor-pointer hover:bg-gray-200 font-bold text-gray-700 py-2 text-center font-sans block">پنل مدیریت</button>
+                                                <Link href={'/user/favorites'} >
+                                                    <a className="text-xs cursor-pointer hover:bg-gray-200 font-bold text-gray-700 py-2 text-center font-sans block">محبوب‌ها</a>
+                                                </Link>
+                                                <Link href={'/user/history'} >
+                                                    <a className="text-xs cursor-pointer hover:bg-gray-200 font-bold text-gray-700 py-2 text-center font-sans block">مشاهدات اخیر</a>
+                                                </Link>
+                                                <Link href={'/user/invoices'} >
+                                                    <a className="text-xs cursor-pointer hover:bg-gray-200 font-bold text-gray-700 py-2 text-center font-sans block">سفارشات</a>
+                                                </Link>
+                                            </nav>
+                                        ) : (
+                                            <nav className="w-full">
+                                                <Link href={`/${user.account_type}`} >
+                                                    <a className="text-xs cursor-pointer  hover:bg-gray-200 font-bold text-gray-700 py-2 text-center font-sans block">پنل مدیریت</a>
+                                                </Link>
+                                                <Link href={'/user/favorites'} >
+                                                    <a className="text-xs cursor-pointer hover:bg-gray-200 font-bold text-gray-700 py-2 text-center font-sans block">محبوب‌ها</a>
+                                                </Link>
+                                                <Link href={'/user/history'} >
+                                                    <a className="text-xs cursor-pointer hover:bg-gray-200 font-bold text-gray-700 py-2 text-center font-sans block">مشاهدات اخیر</a>
+                                                </Link>
+                                                <Link href={'/user/invoices'} >
+                                                    <a className="text-xs cursor-pointer hover:bg-gray-200 font-bold text-gray-700 py-2 text-center font-sans block">سفارشات</a>
+                                                </Link>
+                                            </nav>
+                                        )}
+                                        <button onClick={()=> {dispatch(userLogout())}} className="w-full text-xs cursor-pointer hover:bg-red-100  font-bold text-red-600 py-2 text-center font-sans ">خروج</button>
+                                    </div>
+                                )}
+                            </>
+                        ) : (
+                            <>
+                                {loading ? (
+                                    <button className="cursor-default bg-white px-4 py-2 border border-gray-300 rounded-md text-xs w-full font-sans text-gray-700">...</button>
+                                ) : ( 
+                                    <button onClick={() => dispatch(authPanel({isOpen : true,type : "normal"}))} className="bg-white px-4 py-2 w-full border border-gray-300 rounded-md text-xs font-sans text-gray-700">ورود / ثبت نام</button>
+                                )}
+                            </>
+                        )}
+
+                    </section>
 
                </section>
 
