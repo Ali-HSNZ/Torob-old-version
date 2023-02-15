791ff45fd3e485548725c823d98b6eed10bd0a59
Ali Hassanzadeh <aliatraby@gmail.com>
2023-02-15 07:29:25 -0800

    Show History Search in Search Panel in Header =>  src/layout/Header.js

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,6 +1,7 @@
 import BigScreenMenu from "@/common/BigScreenMenu";
 import SmallScreenMenu from "@/common/SmallScreenMenu";
 import Login from "@/components/Login";
+import { disableScroll, enableScroll } from "@/utils/global";
 import { toPersianDigits } from "@/utils/toPersianDigits";
 import { useFormik } from "formik";
 import Link from "next/link";
@@ -10,6 +11,14 @@ import { useSelector, useDispatch } from "react-redux";
 import { userLogout, authPanel } from "src/redux/user/userActions";
 import { requestError } from "src/services/http";
 import images from "/public/torob_logo.svg"
+//! ====Swiper====>
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { FreeMode , Navigation} from "swiper";
+import "swiper/css/free-mode";
+//! <====Swiper====
 
 const Header = () => {
 
@@ -17,25 +26,42 @@ const Header = () => {
      const { user, loading } = useSelector((state) => state.auth);
      const dispatch = useDispatch();
      const {cart_count} = useSelector(state => state.cart)
+     const {data} = useSelector(state => state.home_state)
+
      
      const [isSmallScreenModal , setIsSmallScreenModal] = useState(false)
 
      const {categories} = useSelector(state => state.categories)
 
-     const handleUserPanel_btn = ()=>{
-          if(document){
-               document.addEventListener('click', function handleClickOutsideBox(event) {
-                    const userPhoneNumber_btn = document.querySelectorAll('.userPhoneNumber_btn')[0];
-                    const panel = document.querySelectorAll('.userPanel')[0];
-                    if(userPhoneNumber_btn && panel){
-                         if(userPhoneNumber_btn.contains(event.target)) {
-                              panel.style.display = "block"
-                         }else{
-                              panel.style.display = "none"
-                         }
+     if(typeof document !== 'undefined'){
+          //! User
+          const userPhoneNumber_btn = document.querySelectorAll('.userPhoneNumber_btn')[0];
+          const panel = document.querySelectorAll('.userPanel')[0];
+          //! Search
+          const searchPanel = document.getElementById("searchPanel");
+          const searchInput = document.getElementById("searchInput");
+
+          document.addEventListener('click', function handleClickOutsideBox(event) {
+               //! User Panel
+               if(userPhoneNumber_btn && panel){
+                    if(userPhoneNumber_btn.contains(event.target)) {
+                         panel.style.display = "block"
+                    }else{
+                         panel.style.display = "none"
                     }
-               });
-          }
+               }
+               //! Search Panel
+               if(searchPanel && searchInput){
+                    if(searchInput.contains(event.target) | searchPanel.contains(event.target)) {
+                         disableScroll()
+                         searchPanel.style.display = 'block'
+                    }else{
+                         enableScroll()
+                         searchPanel.style.display = 'none'
+                    }
+               }
+
+          });
      }
 
      const onSubmit = ({title}) => {
@@ -82,12 +108,62 @@ const Header = () => {
 
                     <section className="w-full flex flex-row justify-between ">
                          {/* //? Search Form */}
-                         <form onSubmit={formik.handleSubmit} method="get" className="w-full  relative mt-4 md:mt-0 md:ml-4 md:mr-6  flex sm:justify-center lg:justify-start items-center z-10">
-                              <div className="relative w-full h-auto">
-                                   <input onBlur={formik.handleBlur} onChange={formik.handleChange } name="title" className="pr-12 bg-gray-200 outline-none rounded-md placeholder:text-sm text-sm text-gray-700 py-3 w-full font-sans lg:w-[420px] shadow-sm px-4" value={formik.values.title} placeholder="جستجو" />
+                         <form onSubmit={formik.handleSubmit} method="get" className="w-full mt-4 md:mt-0 md:ml-4 md:mr-6  flex sm:justify-center lg:justify-start items-center z-10">
+                              <div className="relative w-full lg:w-fit h-auto">
+                                   <input id="searchInput" onBlur={formik.handleBlur} onChange={formik.handleChange } name="title" className="pr-12 bg-gray-200 outline-none rounded-md placeholder:text-sm text-sm text-gray-700 py-3 w-full font-sans lg:w-[420px] shadow-sm px-4" value={formik.values.title} placeholder="جستجو" />
                                    <svg className="w-6 h-6 text-gray-500 absolute top-[9px] right-3 " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                                    </svg>
+                                   {/* //? Search Panel */}
+                                   <section id="searchPanel" className="hidden pb-4 bg-white rounded-md absolute w-full">
+                                        {/* //! Popular Search Section */}
+                                        <div className="mt-4 mx-4 w-full flex flex-row items-center">
+                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ml-4 text-gray-500">
+                                                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                             </svg>
+                                             <h5 className="font-sans text-sm text-gray-700  font-bold ">آخرین جستجوهای شما</h5>
+                                        </div>
+                                        {/* //? Slider For Popular Search */}
+                                        <div className="pr-4 mt-2">
+                                             <Swiper className={"popularSearchSlider_header_swiper"} freeMode={true} navigation={true} spaceBetween={3} modules={[ Navigation , FreeMode]}  slidesPerView={'auto'}>
+                                                 {data && data?.search_bar.popular.map(item => (
+                                                       <SwiperSlide key={item.id}>
+                                                            <Link href={`/search?query=${item.text}`}>
+                                                                 <a className="w-fit px-4 py-2 flex font-sans font-bold text-gray-700 text-sm rounded-full border border-gray-200">
+                                                                      <p>{item.text}</p>
+                                                                      <svg className="w-5 h-5 mr-1 text-gray-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                                                                           <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+                                                                      </svg>
+                                                                 </a>
+                                                            </Link>
+                                                       </SwiperSlide>
+                                                 ))}                                     
+                                             </Swiper>
+                                        </div>
+                                        
+                                        {/* //! Popular Search Section */}
+                                        <div className="mt-4 mx-4 w-full flex flex-row items-center">
+                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ml-4 text-gray-500">
+                                                  <path strokeLinecap="round" strokeLinejoin="round" d="M15.362 5.214A8.252 8.252 0 0112 21 8.25 8.25 0 016.038 7.048 8.287 8.287 0 009 9.6a8.983 8.983 0 013.361-6.867 8.21 8.21 0 003 2.48z" />
+                                                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 18a3.75 3.75 0 00.495-7.467 5.99 5.99 0 00-1.925 3.546 5.974 5.974 0 01-2.133-1A3.75 3.75 0 0012 18z" />
+                                             </svg>
+                                             <h5 className="font-sans text-sm text-gray-700  font-bold ">جستجوهای پرطرفدار</h5>
+                                        </div>
+                                        {/* //? Slider For Popular Search */}
+                                        <div className="pr-4 mt-2">
+                                             <Swiper className={"popularSearchSlider_header_swiper"} freeMode={true} navigation={true} spaceBetween={3} modules={[ Navigation , FreeMode]}  slidesPerView={'auto'}>
+                                                  <SwiperSlide>
+                                                       <a href="#" className="w-fit px-4 py-2 flex font-sans font-bold text-gray-700 text-sm rounded-full border border-gray-200">
+                                                            <p>جستجوهای پرطرفدار</p>
+                                                            <svg className="w-5 h-5 mr-1 text-gray-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                                                                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+                                                            </svg>
+                                                       </a>
+                                                  </SwiperSlide>                                                 
+                                             </Swiper>
+                                        </div>
+
+                                   </section>
                               </div>
                          </form>
                          {/* //? Cart & Login Button */}
@@ -104,7 +180,7 @@ const Header = () => {
                               </Link>
                               <section className="relative w-[123px]">
                               {/* Login Button => 0936... */}
-                              {user?.phone_number_primary && <button onClick={() => handleUserPanel_btn()} className="w-full userPhoneNumber_btn whitespace-nowrap rounded-md border  text-gray-800  border-gray-300 bg-white md:py-3 py-2 font-sans text-sm ">
+                              {user?.phone_number_primary && <button  className="w-full userPhoneNumber_btn whitespace-nowrap rounded-md border  text-gray-800  border-gray-300 bg-white md:py-3 py-2 font-sans text-sm ">
                                    {toPersianDigits(user.phone_number_primary)}
                               </button>}
                               {user ? (
@@ -164,7 +240,7 @@ const Header = () => {
                                         {loading ? (
                                              <button className="w-full whitespace-nowrap rounded-md border  text-gray-800  border-gray-300 bg-white px-4 md:py-3 py-2  text-sm">...</button>
                                         ) : (
-                                             <button onClick={() => handleUserPanel_btn() & dispatch(authPanel({isOpen : true,type : "normal"}))} className="w-full whitespace-nowrap rounded-md border  text-gray-800  border-gray-300 bg-white  md:py-3 py-2 font-sans text-sm">ورود / ثبت نام</button>
+                                             <button onClick={() => dispatch(authPanel({isOpen : true,type : "normal"}))} className="w-full whitespace-nowrap rounded-md border  text-gray-800  border-gray-300 bg-white  md:py-3 py-2 font-sans text-sm">ورود / ثبت نام</button>
                                         )}
                                    </>
                               )}
@@ -174,16 +250,6 @@ const Header = () => {
                     </section>
                     {/* //? Input Search =>  */}
                </div>
-
-               {/* //!  Mobile Search Input For Medium With =>  */}
-               {/* <form onSubmit={formik.handleSubmit} method="get" className="w-full flex sm:hidden px-4 sm:px-8 mt-4 sm:justify-center items-center">
-                    <input onBlur={formik.handleBlur} onChange={formik.handleChange } name="title" className="bg-white text-gray-700 rounded-r-md outline-none w-full py-2  font-sans border  border-gray-300 px-4" value={formik.values.title}  placeholder="نام کالا را وارد کنید"/>
-                    <button type="submit" className="bg-[#d73948] py-2 px-5 rounded-l-md  ">
-                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="w-6 h-6">
-                              <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                         </svg>
-                    </button>
-               </form> */}
 
 
                {/* //?  Menu For Big Screen  ==> */}
