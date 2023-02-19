72b376df66d928d0ecae8a1c1dcc44ec513ce2ea
Ali Hassanzadeh <aliatraby@gmail.com>
2023-02-19 10:18:19 -0800

    Show User Search History in Search Panel in Header => src/layout/Header.js

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -6,7 +6,7 @@ import { toPersianDigits } from "@/utils/toPersianDigits";
 import { useFormik } from "formik";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { userLogout, authPanel } from "src/redux/user/userActions";
 import { requestError } from "src/services/http";
@@ -22,11 +22,12 @@ import "swiper/css/free-mode";
 
 const Header = () => {
 
+
      const router = useRouter();
      const { user, loading } = useSelector((state) => state.auth);
      const dispatch = useDispatch();
      const {cart_count} = useSelector(state => state.cart)
-     const {data} = useSelector(state => state.home_state)
+     const {data , error} = useSelector(state => state.home_state)
 
      
      const [isSmallScreenModal , setIsSmallScreenModal] = useState(false)
@@ -53,11 +54,11 @@ const Header = () => {
                //! Search Panel
                if(searchPanel && searchInput){
                     if(searchInput.contains(event.target) | searchPanel.contains(event.target)) {
-                         disableScroll()
+                         // disableScroll()
                          searchPanel.style.display = 'block'
                          searchInput.classList.add('rounded-b-none')
                     }else{
-                         enableScroll()
+                         // enableScroll()
                          searchPanel.style.display = 'none'
                          searchInput.classList.remove('rounded-b-none')
                     }
@@ -119,51 +120,63 @@ const Header = () => {
                                    {/* //! Search Panel */}
                                    <section id="searchPanel" className="hidden pb-4 bg-white shadow-md border border-gray-300 border-t-0 rounded-md rounded-t-sm absolute w-full">
                                         {/* //? Popular Search Section */}
-                                        <div className="mt-4 mx-4 w-full flex flex-row items-center">
-                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ml-4 text-gray-500">
-                                                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                             </svg>
-                                             <h5 className="font-sans text-sm text-gray-700  font-bold ">آخرین جستجوهای شما</h5>
-                                        </div>
-                                        {/* //? Slider For Popular Search */}
-                                        <div className="pr-4 mt-2">
-                                             <Swiper className={"searchSlider_header"} freeMode={true} navigation={true} spaceBetween={3} modules={[ Navigation , FreeMode]}  slidesPerView={'auto'}>
-                                                 {data && data?.search_bar.popular.map(item => (
-                                                       <SwiperSlide key={item.id}>
-                                                            <Link href={`/search?query=${item.text}`}>
-                                                                 <a className="w-fit px-4 py-2 flex font-sans font-bold text-gray-700 text-sm rounded-full border border-gray-200">
-                                                                      <p>{item.text}</p>
-                                                                      <svg className="w-5 h-5 mr-1 text-gray-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-                                                                           <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-                                                                      </svg>
-                                                                 </a>
-                                                            </Link>
-                                                       </SwiperSlide>
-                                                 ))}                                     
-                                             </Swiper>
-                                        </div>
+                                        {data?.search_bar?.user?.length > 0 && (
+                                             <>
+                                                  <div className="mt-4 mx-4 w-full flex flex-row items-center">
+                                                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ml-4 text-gray-500">
+                                                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                                       </svg>
+                                                       <h5 className="font-sans text-sm text-gray-700  font-bold ">آخرین جستجوهای شما</h5>
+                                                  </div>
+                                                  {/* //? Slider For User History Search */}
+                                                  <div className="pr-4 mt-2">
+                                                       <Swiper className={"searchSlider_header"} freeMode={true} navigation={true} spaceBetween={3} modules={[ Navigation , FreeMode]}  slidesPerView={'auto'}>
+                                                            {data.search_bar.user.map((item,index) => (
+                                                                 <SwiperSlide key={index}>
+                                                                      <Link href={{pathname: "/search", query: { query: item }}}>
+                                                                           <a className="w-fit px-4 py-2 flex  rounded-full border border-gray-200">
+                                                                                <p className="font-sans font-bold text-gray-700 text-sm">{item}</p>
+                                                                                <svg className="w-5 h-5 mr-1 text-gray-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                                                                                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+                                                                                </svg>
+                                                                           </a>
+                                                                      </Link>
+                                                                 </SwiperSlide>                                                 
+                                                            ))}
+                                                       </Swiper>
+                                                  </div>
+                                             </>
+                                        )}
                                         
-                                        {/* //? Popular Search Section */}
-                                        <div className="mt-4 mx-4 w-full flex flex-row items-center">
-                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ml-4 text-gray-500">
-                                                  <path strokeLinecap="round" strokeLinejoin="round" d="M15.362 5.214A8.252 8.252 0 0112 21 8.25 8.25 0 016.038 7.048 8.287 8.287 0 009 9.6a8.983 8.983 0 013.361-6.867 8.21 8.21 0 003 2.48z" />
-                                                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 18a3.75 3.75 0 00.495-7.467 5.99 5.99 0 00-1.925 3.546 5.974 5.974 0 01-2.133-1A3.75 3.75 0 0012 18z" />
-                                             </svg>
-                                             <h5 className="font-sans text-sm text-gray-700  font-bold ">جستجوهای پرطرفدار</h5>
-                                        </div>
-                                        {/* //? Slider For Popular Search */}
-                                        <div className="pr-4 mt-2">
-                                             <Swiper className={"searchSlider_header"} freeMode={true} navigation={true} spaceBetween={3} modules={[ Navigation , FreeMode]}  slidesPerView={'auto'}>
-                                                  <SwiperSlide>
-                                                       <a href="#" className="w-fit px-4 py-2 flex font-sans font-bold text-gray-700 text-sm rounded-full border border-gray-200">
-                                                            <p>جستجوهای پرطرفدار</p>
-                                                            <svg className="w-5 h-5 mr-1 text-gray-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-                                                                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-                                                            </svg>
-                                                       </a>
-                                                  </SwiperSlide>                                                 
-                                             </Swiper>
-                                        </div>
+                                        {data?.search_bar?.popular?.length > 0 && (
+                                             <>
+                                                  {/* //? Title */}
+                                                  <div className="mt-4 mx-4 w-full flex flex-row items-center">
+                                                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ml-4 text-gray-500">
+                                                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.362 5.214A8.252 8.252 0 0112 21 8.25 8.25 0 016.038 7.048 8.287 8.287 0 009 9.6a8.983 8.983 0 013.361-6.867 8.21 8.21 0 003 2.48z" />
+                                                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 18a3.75 3.75 0 00.495-7.467 5.99 5.99 0 00-1.925 3.546 5.974 5.974 0 01-2.133-1A3.75 3.75 0 0012 18z" />
+                                                       </svg>
+                                                       <h5 className="font-sans text-sm text-gray-700  font-bold ">جستجوهای پرطرفدار</h5>
+                                                  </div>
+                                                  {/* //? Slider For Popular Search */}
+                                                  <div className="pr-4 mt-2">
+                                                       <Swiper className={"searchSlider_header"} freeMode={true} navigation={true} spaceBetween={3} modules={[ Navigation , FreeMode]}  slidesPerView={'auto'}>
+                                                       {data.search_bar.popular.map((item,index) => (
+                                                                 <SwiperSlide key={index}>
+                                                                      <Link href={{pathname: "/search", query: { query: item }}}>
+                                                                           <a className="w-fit px-4 py-2 flex font-sans font-bold text-gray-700 text-sm rounded-full border border-gray-200">
+                                                                                <p>{item}</p>
+                                                                                <svg className="w-5 h-5 mr-1 text-gray-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                                                                                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+                                                                                </svg>
+                                                                           </a>
+                                                                      </Link>
+                                                                 </SwiperSlide>
+                                                       ))}                                     
+                                                       </Swiper>
+                                                  </div>
+                                             </>
+                                        )}
 
                                    </section>
                               </div>
