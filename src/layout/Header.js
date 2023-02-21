9324868b03bf43dd60a8abd780a230ecff5c9cf4
Ali Hassanzadeh <aliatraby@gmail.com>
2023-02-21 23:18:39 +0330

    Use 'deleteUserSearch' Action in 'delete user history search' in header  => src/layout/Header.js

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -18,7 +18,7 @@ import "swiper/css/pagination";
 import { FreeMode , Navigation} from "swiper";
 import "swiper/css/free-mode";
 //! <====Swiper====
-import { fetchUserSearchSuggested } from "@/redux/userSearch/userSaerch_actions";
+import { deleteUserSearch, fetchUserSearchSuggested } from "@/redux/userSearch/userSaerch_actions";
 
 const Header = () => {
 
@@ -166,13 +166,20 @@ const Header = () => {
                                              {/* //? User History Search */}
                                                   {searchData?.data?.search_bar?.user?.length > 0 && (
                                                        <>
-                                                            <div className="mt-4 mx-4 w-full flex flex-row items-center">
-                                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ml-4 text-gray-500">
-                                                                      <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                                                 </svg>
-                                                                 <h5 className="font-sans text-sm text-gray-700  font-bold ">آخرین جستجوهای شما</h5>
+                                                            <div className="mt-4 px-4  w-full flex flex-row justify-between items-center">
+                                                                 <div className="flex w-fit items-center">
+                                                                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ml-4 text-gray-500">
+                                                                           <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                                                      </svg>
+                                                                      <h5 className="font-sans text-sm text-gray-700  font-bold ">آخرین جستجوهای شما</h5>
+                                                                 </div>
+                                                                 <button className="p-1" onClick={() => dispatch(deleteUserSearch())} >
+                                                                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-gray-500">
+                                                                           <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                                                      </svg>
+                                                                 </button>
                                                             </div>
-                                                            <div className="pr-4 mt-2">
+                                                            <div className="pr-4 mt-3">
                                                                  <Swiper className={"searchSlider_header"} freeMode={true} navigation={true} spaceBetween={3} modules={[ Navigation , FreeMode]}  slidesPerView={'auto'}>
                                                                       {searchData.data.search_bar.user.map((item,index) => (
                                                                            <SwiperSlide key={index}>
