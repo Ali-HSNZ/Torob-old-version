c9f7e75ebf5e41420799c1b1dc0d53f0fd78bd19
Ali Hassanzadeh <aliatraby@gmail.com>
2023-04-09 23:46:20 +0430

    Change Sort Button Styles in Search Page

--- src/components/searchPage/main/BreadCrumpAndSort.js
+++ src/components/searchPage/main/BreadCrumpAndSort.js
@@ -7,7 +7,7 @@ import React, { useState } from 'react';
 
 const BreadCrumpAndSort = ({category , similarCategories}) => {
 
-    const [currentFilter,setCurrentFilter] = useState("جدید ترین")
+    const [currentFilter,setCurrentFilter] = useState("جدیدترین")
     const router = useRouter()
     const {query} = useRouter()
     
@@ -28,7 +28,7 @@ const BreadCrumpAndSort = ({category , similarCategories}) => {
                 {/* //? Sort */}
                 <div className="max-w-fit py-2 relative  cursor-pointer hidden  md:flex flex-col justify-end  items-end">
                 
-                    <Button sx={{fontFamily : 'iranyekan' , fontSize : "14px", color : '#545353'}} id="basic-button" aria-controls={open ? 'basic-menu' : undefined} aria-haspopup="true" aria-expanded={open ? 'true' : undefined} onClick={handleClick} className="rounded-md text-sm">
+                    <Button sx={{fontFamily : 'iranyekan-medium' , fontSize : "14px", color : '#545353'}} id="basic-button" aria-controls={open ? 'basic-menu' : undefined} aria-haspopup="true" aria-expanded={open ? 'true' : undefined} onClick={handleClick} className="rounded-md text-sm">
                         {currentFilter}
                         <svg  className={`w-5 h-5 mr-2 ${anchorEl ? "rotate-180" : ""}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
@@ -36,17 +36,17 @@ const BreadCrumpAndSort = ({category , similarCategories}) => {
                     </Button>
                     <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
                         <div className="flex flex-col">
-                            <section  onClick={() => router.push({pathname : '/search' , query : {...query , sort : "mostFavorite"}}) &  handleClose() & setCurrentFilter("محبوب‌ ترین")}>
-                                <p className={`${query.sort && query.sort === "mostFavorite" ? "bg-gray-200" : ""} py-1.5 px-4 w-full font-iranyekan-regular cursor-pointer text-sm text-gray-600 text-center hover:bg-gray-100`}>محبوب‌ ترین</p>
+                            <section  onClick={() => router.push({pathname : '/search' , query : {...query , sort : "mostFavorite"}}) &  handleClose() & setCurrentFilter("محبوب‌‌ترین")}>
+                                <p className={`${query.sort && query.sort === "mostFavorite" ? "bg-gray-200" : ""} py-1.5 px-4 w-full font-iranyekan-regular cursor-pointer text-sm text-gray-600 text-center hover:bg-gray-100`}>محبوب‌‌ترین</p>
                             </section>
-                            <section onClick={() => router.push({pathname : '/search' , query : {...query , sort : "dateRecent"}}) & handleClose() & setCurrentFilter("جدید ترین")}>
-                                <p className={`${query.sort && query.sort === "dateRecent" ? "bg-gray-200" : ""} py-1.5 px-4 w-full font-iranyekan-regular cursor-pointer text-sm text-gray-600 text-center hover:bg-gray-100`}>جدید ترین</p>
+                            <section onClick={() => router.push({pathname : '/search' , query : {...query , sort : "dateRecent"}}) & handleClose() & setCurrentFilter("جدید‌ترین")}>
+                                <p className={`${query.sort && query.sort === "dateRecent" ? "bg-gray-200" : ""} py-1.5 px-4 w-full font-iranyekan-regular cursor-pointer text-sm text-gray-600 text-center hover:bg-gray-100`}>جدید‌ترین</p>
                             </section>
-                            <section onClick={() =>  router.push({pathname : '/search' , query : {...query , sort : "priceMin"}}) & handleClose() & setCurrentFilter("ارزان ترین")}>
-                                <p className={`${query.sort && query.sort === "priceMin" ? "bg-gray-200" : ""} py-1.5 px-4 w-full font-iranyekan-regular cursor-pointer text-sm text-gray-600 text-center hover:bg-gray-100`}>ارزان ترین</p>
+                            <section onClick={() =>  router.push({pathname : '/search' , query : {...query , sort : "priceMin"}}) & handleClose() & setCurrentFilter("ارزان‌ترین")}>
+                                <p className={`${query.sort && query.sort === "priceMin" ? "bg-gray-200" : ""} py-1.5 px-4 w-full font-iranyekan-regular cursor-pointer text-sm text-gray-600 text-center hover:bg-gray-100`}>ارزان‌ترین</p>
                             </section>
-                            <section onClick={() => router.push({pathname : '/search' , query : {...query , sort : "priceMax"}}) & handleClose() & setCurrentFilter("گران ترین")}>
-                                <p className={`${query.sort && query.sort === "priceMax" ? "bg-gray-200" : ""} py-1.5 px-4 w-full font-iranyekan-regular cursor-pointer text-sm text-gray-600 text-center hover:bg-gray-100`}>گران ترین</p>
+                            <section onClick={() => router.push({pathname : '/search' , query : {...query , sort : "priceMax"}}) & handleClose() & setCurrentFilter("گران‌ترین")}>
+                                <p className={`${query.sort && query.sort === "priceMax" ? "bg-gray-200" : ""} py-1.5 px-4 w-full font-iranyekan-regular cursor-pointer text-sm text-gray-600 text-center hover:bg-gray-100`}>گران‌ترین</p>
                             </section>
                         </div>
                     </Menu>
