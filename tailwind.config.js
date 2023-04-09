8207077a175400998af6a0486e60b71a4f7aa88d
Ali Hassanzadeh <aliatraby@gmail.com>
2023-04-09 21:04:36 +0430

    Refactor 'tailwind configs' - Remove flowbite react  => tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./node_modules/flowbite-react/**/*.js",
-    "src/pages/**/*.{js,ts,jsx,tsx}",
-    "src/components/**/*.{js,ts,jsx,tsx}",
-    "src/common/**/*.{js,ts,jsx,tsx}",
-    "src/layout/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     screens: {
@@ -38,7 +34,4 @@ module.exports = {
       }
     },
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
 }
