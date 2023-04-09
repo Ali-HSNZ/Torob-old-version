14c2c15a632547526fa0a0b3b02f5a5aa8bcf65d
Ali Hassanzadeh <aliatraby@gmail.com>
2023-04-09 22:40:59 +0430

    #5 Change FontFamily - in All Project Files

--- src/components/userPage/Aside.js
+++ src/components/userPage/Aside.js
@@ -32,7 +32,7 @@ const UserPageAside = ({isMobileScreen,setIsMobileScreen,mobileScreenClassName})
                               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 " >
                                    <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"/>
                               </svg>
-                              <span className="font-iranyekan-regular mr-2  font-iranyekan-bold text-sm ">محبوب‌ها</span>
+                              <span className="mr-2  font-iranyekan-bold text-sm ">محبوب‌ها</span>
                          </a>
                     </Link>
                     <Link href={"/user/history"}>
@@ -40,7 +40,7 @@ const UserPageAside = ({isMobileScreen,setIsMobileScreen,mobileScreenClassName})
                               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"/>
                               </svg>
-                              <span className="font-iranyekan-regular mr-2  font-iranyekan-bold text-sm ">مشاهدات اخیر</span>
+                              <span className="mr-2  font-iranyekan-bold text-sm ">مشاهدات اخیر</span>
                          </a>
                     </Link>
                     <Link href={"/user/invoices"}>
@@ -48,7 +48,7 @@ const UserPageAside = ({isMobileScreen,setIsMobileScreen,mobileScreenClassName})
                               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
                               </svg>
-                              <span className="font-iranyekan-regular mr-2  font-iranyekan-bold text-sm ">سفارشات</span>
+                              <span className="mr-2  font-iranyekan-bold text-sm ">سفارشات</span>
                          </a>
                     </Link>
                </nav>
